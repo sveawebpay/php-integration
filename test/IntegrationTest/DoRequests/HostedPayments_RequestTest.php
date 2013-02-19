b2e88586c68ebf8042fe1dbe716731bbae30110a
Daniel Brolund <daniel.brolund@sveaekonomi.se>
2013-02-19 15:49:17 +0100

    Replaced backslash with slash in paths

--- test/IntegrationTest/DoRequests/HostedPayments_RequestTest.php
+++ test/IntegrationTest/DoRequests/HostedPayments_RequestTest.php
@@ -1,7 +1,7 @@
 <?php
 
 $root = realpath(dirname(__FILE__));
-require_once $root . '\..\..\..\src\Includes.php';
+require_once $root . '/../../../src/Includes.php';
 
 $root = realpath(dirname(__FILE__));
 require_once $root . '/../../UnitTest/BuildOrder/TestRowFactory.php';
