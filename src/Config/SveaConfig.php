3e9c29c091bb9467122cbf1b3898d9c6ce9e10a5
Anneli Halldn <anneli.halldn@sveaekonomi.se>
2013-02-28 10:53:03 +0100

    changed to default values in sveaconfig and added it to gitignore

--- src/Config/SveaConfig.php
+++ src/Config/SveaConfig.php
@@ -29,14 +29,9 @@ class SveaConfig {
         $this->password = 'sverigetest';
         $this->invoiceClientnumber = 79021;
         $this->paymentPlanClientnumber = 59999;
-        $this->merchantId = 1175;
-        $this->secret = "d153477288051d6001adf0648405e0fcfaa3ee2a8dc90dd3151341a1d68b1a4388616585fe7bc15cd06882070b0d92aa92de6cde1e7a21dc7e65e81cee6af43f";
-       
-        /**
         $this->merchantId = 1200;
         $this->secret = "27f18bfcbe4d7f39971cb3460fbe7234a82fb48f985cf22a068fa1a685fe7e6f93c7d0d92fee4e8fd7dc0c9f11e2507300e675220ee85679afa681407ee2416d";
-         * 
-         */
+       
     }
 
     /**
