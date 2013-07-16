b8ab842d783feda98ad15a9f9c17ee2249b27d51
Jonas Lith <jonas.lith@sveaekonomi.se>
2013-07-16 11:19:57 +0200

    Moved another test to GetPaymentPlanParamsTest

--- test/IntegrationTest/Response/WebServiceResponseTest.php
+++ test/IntegrationTest/Response/WebServiceResponseTest.php
@@ -51,28 +51,6 @@ class WebServiceResponseTest extends PHPUnit_Framework_TestCase {
         return $request->sveaOrderId;
     }
     
-    function testResultGetPaymentPlanParams() {
-        $addressRequest = WebPay::getPaymentPlanParams();
-        $request = $addressRequest
-                //->setTestmode()()
-                ->setCountryCode("SE")
-                ->doRequest();
-        
-        $this->assertEquals(1, $request->accepted);
-        $this->assertEquals(0, $request->resultcode);
-        $this->assertEquals(213060, $request->campaignCodes[0]->campaignCode);
-        $this->assertEquals('Köp nu betala om 3 månader (räntefritt)', $request->campaignCodes[0]->description);
-        $this->assertEquals('InterestAndAmortizationFree', $request->campaignCodes[0]->paymentPlanType);
-        $this->assertEquals(3, $request->campaignCodes[0]->contractLengthInMonths);
-        $this->assertEquals(100, $request->campaignCodes[0]->initialFee);
-        $this->assertEquals(29, $request->campaignCodes[0]->notificationFee);
-        $this->assertEquals(0, $request->campaignCodes[0]->interestRatePercent);
-        $this->assertEquals(3, $request->campaignCodes[0]->numberOfInterestFreeMonths);
-        $this->assertEquals(3, $request->campaignCodes[0]->numberOfPaymentFreeMonths);
-        $this->assertEquals(1000, $request->campaignCodes[0]->fromAmount);
-        $this->assertEquals(50000, $request->campaignCodes[0]->toAmount);
-    }
-    
      /**
      * @expectedException Exception
      * @expectedExceptionMessage Invalid or missing Country code
