3d9f3fdabcef85884b9d6955d3ad5bd41761bda1
Jonas Lith <jonas.lith@sveaekonomi.se>
2013-07-16 13:43:09 +0200

    Deleted another unused function

--- test/IntegrationTest/Response/WebServiceResponseTest.php
+++ test/IntegrationTest/Response/WebServiceResponseTest.php
@@ -9,35 +9,6 @@ require_once $root . '/../../../src/Includes.php';
  * @author Anneli Halld'n, Daniel Brolund for Svea Webpay
  */
 class WebServiceResponseTest extends PHPUnit_Framework_TestCase {
-
-    /**
-     * Function to use in testfunctions
-     * @return SveaOrderId
-     */
-    function getInvoiceOrderId() {
-        $request = WebPay::createOrder()
-                //->setTestmode()()
-                ->addOrderRow(Item::orderRow()
-                    ->setArticleNumber(1)
-                    ->setQuantity(2)
-                    ->setAmountExVat(100.00)
-                    ->setDescription("Specification")
-                    ->setName('Prod')
-                    ->setUnit("st")
-                    ->setVatPercent(25)
-                    ->setDiscountPercent(0)
-                        )
-                ->addCustomerDetails(Item::individualCustomer()->setNationalIdNumber(194605092222))
-                ->setCountryCode("SE")
-                ->setCustomerReference("33")
-                ->setOrderDate("2012-12-12")
-                ->setCurrency("SEK")
-                ->useInvoicePayment()// returnerar InvoiceOrder object
-                //->setPasswordBasedAuthorization("sverigetest", "sverigetest", 79021)
-                ->doRequest();
-        
-        return $request->sveaOrderId;
-    }
     
     /**
      * @expectedException Exception
