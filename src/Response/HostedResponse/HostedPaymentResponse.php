6c4e5ed559eaf0a604f4fbfed3a200be95db9c04
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2013-09-23 14:13:21 +0200

    Cleaned up some code in HostedResponse for increased readability.

--- src/Response/HostedResponse/HostedPaymentResponse.php
+++ src/Response/HostedResponse/HostedPaymentResponse.php
@@ -18,19 +18,19 @@ class HostedPaymentResponse extends HostedResponse{
 
     function __construct($response,$countryCode,$config) {
         if (is_array($response)) {
-            if (array_key_exists("mac",$response)&& array_key_exists("response",$response)) {
+            if (array_key_exists("mac",$response) && array_key_exists("response",$response)) {
                 $decodedXml = base64_decode($response["response"]);
                 $secret = $config->getSecret(\ConfigurationProvider::HOSTED_TYPE,$countryCode);
                 if ($this->validateMac($response["response"],$response['mac'],$secret)) {
-                    $this->formatXml($decodedXml);
+                    $this->formatXml($decodedXml); // sets ->accepted, but not ->resultcode
                 } else {
                     $this->accepted = 0;
                     $this->resultcode = '0';
                     $this->errormessage = "Response failed authorization. MAC not valid.";
                 }
             }
-
-        } else {
+        } 
+        else {
             $this->accepted = 0;
             $this->resultcode = '0';
             $this->errormessage = "Response is not recognized.";
@@ -38,7 +38,10 @@ class HostedPaymentResponse extends HostedResponse{
     }
 
     protected function formatXml($xml) {
+        
         $xmlElement = new \SimpleXMLElement($xml);
+
+        // we set accepted iff xml statuscode is 0;
         if ((string)$xmlElement->statuscode == 0) {
             $this->accepted = 1;
         } else {
@@ -46,29 +49,25 @@ class HostedPaymentResponse extends HostedResponse{
             $this->setErrorParams($xmlElement->statuscode);
         }
 
-            $this->transactionId = (string)$xmlElement->transaction['id'];
-            $this->paymentMethod = (string)$xmlElement->transaction->paymentmethod;
-            $this->merchantId = (string)$xmlElement->transaction->merchantid;
-            $this->clientOrderNumber = (string)$xmlElement->transaction->customerrefno;
-            $minorAmount = (int)($xmlElement->transaction->amount);
-            $this->amount = $minorAmount * 0.01;
-            $this->currency = (string)$xmlElement->transaction->currency;
+        $this->transactionId = (string)$xmlElement->transaction['id'];
+        $this->paymentMethod = (string)$xmlElement->transaction->paymentmethod;
+        $this->merchantId = (string)$xmlElement->transaction->merchantid;
+        $this->clientOrderNumber = (string)$xmlElement->transaction->customerrefno;
+        $minorAmount = (int)($xmlElement->transaction->amount);
+        $this->amount = $minorAmount * 0.01;
+        $this->currency = (string)$xmlElement->transaction->currency;
 
-            if (property_exists($xmlElement->transaction, "subscriptionid")) {
-                $this->subscriptionId = (string)$xmlElement->transaction->subscriptionid;
-                $this->subscriptionType = (string)$xmlElement->transaction->subscriptiontype;
-            }
+        if (property_exists($xmlElement->transaction, "subscriptionid")) {
+            $this->subscriptionId = (string)$xmlElement->transaction->subscriptionid;
+            $this->subscriptionType = (string)$xmlElement->transaction->subscriptiontype;
+        }
 
-            if (property_exists($xmlElement->transaction, "cardtype")) {
-               $this->cardType = (string)$xmlElement->transaction->cardtype;
-               $this->maskedCardNumber = (string)$xmlElement->transaction->maskedcardno;
-               $this->expiryMonth = (string)$xmlElement->transaction->expirymonth;
-               $this->expiryYear = (string)$xmlElement->transaction->expiryyear;
-               $this->authCode = (string)$xmlElement->transaction->authcode;
-            }
-
-
+        if (property_exists($xmlElement->transaction, "cardtype")) {
+           $this->cardType = (string)$xmlElement->transaction->cardtype;
+           $this->maskedCardNumber = (string)$xmlElement->transaction->maskedcardno;
+           $this->expiryMonth = (string)$xmlElement->transaction->expirymonth;
+           $this->expiryYear = (string)$xmlElement->transaction->expiryyear;
+           $this->authCode = (string)$xmlElement->transaction->authcode;
+        }
     }
-
-
 }
--- src/Response/SveaResponse.php
+++ src/Response/SveaResponse.php
@@ -9,26 +9,36 @@ class SveaResponse {
     public $response;
 
     public function __construct($message, $countryCode,$config = NULL) {
-         $config = $config == null ? Svea\SveaConfig::getDefaultConfig() : $config;
+         
+        $config = $config == null ? Svea\SveaConfig::getDefaultConfig() : $config;
+        
         if (is_object($message)) {
+            
             if (property_exists($message, "CreateOrderEuResult")) {
                 $this->response = new Svea\CreateOrderResponse($message);
-            } elseif (property_exists($message, "GetAddressesResult")) {
+            } 
+            elseif (property_exists($message, "GetAddressesResult")) {
                 $this->response = new Svea\GetAddressesResponse($message);
-            } elseif (property_exists($message, "GetPaymentPlanParamsEuResult")) {
+            } 
+            elseif (property_exists($message, "GetPaymentPlanParamsEuResult")) {
                 $this->response = new Svea\PaymentPlanParamsResponse($message);
-            } elseif (property_exists($message, "DeliverOrderEuResult")) {
+            } 
+            elseif (property_exists($message, "DeliverOrderEuResult")) {
                 $this->response = new Svea\DeliverOrderResult($message);
-            } elseif (property_exists($message, "CloseOrderEuResult")) {
+            } 
+            elseif (property_exists($message, "CloseOrderEuResult")) {
                 $this->response = new Svea\CloseOrderResult($message);
-                //webservice from hosted_admin
-            }elseif(property_exists($message, "message")){
+            }
+            //webservice from hosted_admin
+            elseif (property_exists($message, "message"))   {
                  $this->response = new Svea\HostedAdminResponse($message,$countryCode,$config);
             }
 
-        } elseif ($message != NULL) {
+        } 
+        elseif ($message != NULL) {
             $this->response = new Svea\HostedPaymentResponse($message,$countryCode,$config);
-        } else {
+        } 
+        else {
             $this->response = "Response is not recognized.";
         }
     }
