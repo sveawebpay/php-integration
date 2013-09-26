8a94b980f19a8077bfd0e76c4520a55d507d02fa
jona-lit <jonas.lith@sveaekonomi.se>
2013-09-26 14:21:50 +0200

    Reordered xml of customer and order rows for hosted payments

--- src/HostedRequests/Helper/HostedXmlBuilder.php
+++ src/HostedRequests/Helper/HostedXmlBuilder.php
@@ -60,74 +60,6 @@ class HostedXmlBuilder {
         return $this->XMLWriter->flush();
     }
 
-    private function serializeOrderRows($orderRows) {
-        if (count($orderRows) > 0) {
-            $this->XMLWriter->startElement("orderrows");
-
-            foreach ($orderRows as $orderRow) {
-                $this->serializeOrderRow($orderRow);
-            }
-
-            $this->XMLWriter->endElement();
-        }
-    }
-
-    private function serializeExcludePayments($payMethods) {
-        if (count($payMethods) > 0) {
-            $this->XMLWriter->startElement("excludepaymentmethods");
-
-            foreach ($payMethods as $payMethod) {
-                $this->XMLWriter->writeElement('exclude', $payMethod);
-            }
-
-            $this->XMLWriter->endElement();
-        }
-    }
-
-    private function serializeOrderRow($orderRow) {
-        $this->XMLWriter->startElement("row");
-
-        if (!empty($orderRow->description) && $orderRow->description != null) {
-            $this->XMLWriter->writeElement("description", $orderRow->description);
-        } else {
-            $this->XMLWriter->writeElement("description", "");
-        }
-
-        if (!empty($orderRow->name) && $orderRow->name != null) {
-            $this->XMLWriter->writeElement("name", $orderRow->name);
-        } else {
-            $this->XMLWriter->writeElement("name", "");
-        }
-
-        if (!empty($orderRow->sku) && $orderRow->sku != null) {
-            $this->XMLWriter->writeElement("sku", $orderRow->sku);
-        } else {
-            $this->XMLWriter->writeElement("sku", "");
-        }
-
-        if (!empty($orderRow->amount) && $orderRow->amount != null) {
-            $this->XMLWriter->writeElement("amount", round($orderRow->amount));
-        } else {
-              $this->XMLWriter->writeElement("amount", "0");
-        }
-
-        if (!empty($orderRow->vat) && $orderRow->vat != null) {
-            $this->XMLWriter->writeElement("vat", round($orderRow->vat));
-        } else {
-            $this->XMLWriter->writeElement("vat", "0");
-        }
-
-        if (!empty($orderRow->unit) && $orderRow->unit != null) {
-            $this->XMLWriter->writeElement("unit", $orderRow->unit);
-        }
-
-        if (!empty($orderRow->quantity) && $orderRow->quantity != null) {
-            $this->XMLWriter->writeElement("quantity", $orderRow->quantity);
-        }
-
-        $this->XMLWriter->endElement();
-    }
-
     private function serializeCustomer($order,$request) {
         $this->XMLWriter->startElement("customer");
         //nordic country individual
@@ -151,12 +83,12 @@ class HostedXmlBuilder {
              $this->XMLWriter->writeElement("initials", $order->customerIdentity->initials);
         }
 
-        if (isset($order->customerIdentity->email)) {
-             $this->XMLWriter->writeElement("email", $order->customerIdentity->email);
-        }
-
         if (isset($order->customerIdentity->phonenumber)) {
              $this->XMLWriter->writeElement("phone", $order->customerIdentity->phonenumber);
+        }
+
+        if (isset($order->customerIdentity->email)) {
+             $this->XMLWriter->writeElement("email", $order->customerIdentity->email);
         }
 
         if (isset($order->customerIdentity->street)) {
@@ -179,7 +111,6 @@ class HostedXmlBuilder {
             $this->XMLWriter->writeElement("city", $order->customerIdentity->locality);
         }
 
-        //country
         if (isset($order->countryCode)) {
             $this->XMLWriter->writeElement("country", $order->countryCode);
         }
@@ -198,6 +129,74 @@ class HostedXmlBuilder {
 
         if (isset($order->customerIdentity->addressSelector)) {
              $this->XMLWriter->writeElement("addressid", $order->customerIdentity->addressSelector);
+        }
+    }
+
+    private function serializeOrderRows($orderRows) {
+        if (count($orderRows) > 0) {
+            $this->XMLWriter->startElement("orderrows");
+
+            foreach ($orderRows as $orderRow) {
+                $this->serializeOrderRow($orderRow);
+            }
+
+            $this->XMLWriter->endElement();
+        }
+    }
+
+    private function serializeOrderRow($orderRow) {
+        $this->XMLWriter->startElement("row");
+
+        if (!empty($orderRow->sku) && $orderRow->sku != null) {
+            $this->XMLWriter->writeElement("sku", $orderRow->sku);
+        } else {
+            $this->XMLWriter->writeElement("sku", "");
+        }
+
+        if (!empty($orderRow->name) && $orderRow->name != null) {
+            $this->XMLWriter->writeElement("name", $orderRow->name);
+        } else {
+            $this->XMLWriter->writeElement("name", "");
+        }
+
+        if (!empty($orderRow->description) && $orderRow->description != null) {
+            $this->XMLWriter->writeElement("description", $orderRow->description);
+        } else {
+            $this->XMLWriter->writeElement("description", "");
+        }
+
+        if (!empty($orderRow->amount) && $orderRow->amount != null) {
+            $this->XMLWriter->writeElement("amount", round($orderRow->amount));
+        } else {
+              $this->XMLWriter->writeElement("amount", "0");
+        }
+
+        if (!empty($orderRow->vat) && $orderRow->vat != null) {
+            $this->XMLWriter->writeElement("vat", round($orderRow->vat));
+        } else {
+            $this->XMLWriter->writeElement("vat", "0");
+        }
+
+        if (!empty($orderRow->quantity) && $orderRow->quantity != null) {
+            $this->XMLWriter->writeElement("quantity", $orderRow->quantity);
+        }
+
+        if (!empty($orderRow->unit) && $orderRow->unit != null) {
+            $this->XMLWriter->writeElement("unit", $orderRow->unit);
+        }
+
+        $this->XMLWriter->endElement();
+    }
+
+    private function serializeExcludePayments($payMethods) {
+        if (count($payMethods) > 0) {
+            $this->XMLWriter->startElement("excludepaymentmethods");
+
+            foreach ($payMethods as $payMethod) {
+                $this->XMLWriter->writeElement('exclude', $payMethod);
+            }
+
+            $this->XMLWriter->endElement();
         }
     }
 
