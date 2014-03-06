d9a8b274e9064228628007c70e644e0fb236bedd
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-03-06 13:07:49 +0100

    added failing inital unit test for CreditTransaction

---
+++ test/UnitTest/Hosted/HandleOrder/CreditTransactionTest.php
@@ -0,0 +1,21 @@
+<?php
+$root = realpath(dirname(__FILE__));
+
+require_once $root . '/../../../../src/Includes.php';
+require_once $root . '/../../../../src/WebServiceRequests/svea_soap/SveaSoapConfig.php';
+
+/**
+ * @author Kristian Grossman-Madsen for Svea Webpay
+ */
+class CreditTransactionTest extends PHPUnit_Framework_TestCase {
+
+    function test_class_exists(){
+        $config = Svea\SveaConfig::getDefaultConfig();
+
+        $creditObject = WebPay::creditTransaction($config);
+        
+        assertInstanceOf( Svea\CreditTransaction, $creditObject);      
+    }
+}
+
+?>
