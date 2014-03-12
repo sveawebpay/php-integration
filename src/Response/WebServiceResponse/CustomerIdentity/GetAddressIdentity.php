fa0d8c5a2b05bcdcd07dc6414b5fb794f9fcb9da
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-03-12 17:34:41 +0100

    Refactored GetAddressesResponse to use constructors instead of attribute injection

--- src/Response/WebServiceResponse/CustomerIdentity/GetAddressIdentity.php
+++ src/Response/WebServiceResponse/CustomerIdentity/GetAddressIdentity.php
@@ -6,12 +6,22 @@ require_once 'CustomerIdentityResponse.php';
 /**
  *  GetAddressIdentity structure
  *
- *  @attrib     $addressSelector    // not guaranteed to be defined
+ *  @attrib $addressSelector
+ *  
+ *  The following are not guaranteed to be set
+ *  @property $firstName
+ *  @property $lastName
  *
  *  @author anne-hal, Kristian Grossman-Madsen
  */
 class GetAddressIdentity extends CustomerIdentityResponse {
 
-    public $addressSelector;    // not guaranteed to be defined
+    public $addressSelector;
+    //public $firstName;
+    //public $lastName;
 
+    function __construct( $customer ) {
+        $this->addressSelector = isset($customer->AddressSelector) ? $customer->AddressSelector : "";        
+        parent::__construct( $customer );
+    }
 }
