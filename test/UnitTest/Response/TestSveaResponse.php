cb061e5b73b57e36f2cfa87c99f2ebf97f8c4f9d
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-03-17 16:12:43 +0100

    tests for all entry modes in SveaResponse added

--- test/UnitTest/Response/TestSveaResponse.php
+++ test/UnitTest/Response/TestSveaResponse.php
@@ -1,56 +1,42 @@
 <?php
-$root = realpath(dirname(__FILE__));
+namespace Svea;
 
+$root = realpath(dirname(__FILE__));
 require_once $root . '/../../../src/Includes.php';
 require_once $root . '/../../../src/WebServiceRequests/svea_soap/SveaSoapConfig.php';
+
 
 /**
  * @author Kristian Grossman-Madsen for Svea Webpay
  */
-class SveaResponseTest extends PHPUnit_Framework_TestCase {
+class SveaResponseTest extends \PHPUnit_Framework_TestCase {
 
-    public function test_handles_null_response() {
-        $config = Svea\SveaConfig::getDefaultConfig();
+    public function test_handles_response_is_null() {
+        $config = SveaConfig::getDefaultConfig();
 
-        $response = new SveaResponse( NULL, "SE", $config );
+        $response = new \SveaResponse( NULL, "SE", $config );
         $this->assertInternalType('string', $response->getResponse() );
         $this->assertEquals('Response is not recognized.', $response->getResponse() );
     }
-}
     
-//    public function __construct($message, $countryCode, $config = NULL) {
-//         
-//        $config = $config == null ? Svea\SveaConfig::getDefaultConfig() : $config;
-//        
-//        if (is_object($message)) {
-//            
-//            if (property_exists($message, "CreateOrderEuResult")) {
-//                $this->response = new Svea\CreateOrderResponse($message);
-//            } 
-//            elseif (property_exists($message, "GetAddressesResult")) {
-//                $this->response = new Svea\GetAddressesResponse($message);
-//            } 
-//            elseif (property_exists($message, "GetPaymentPlanParamsEuResult")) {
-//                $this->response = new Svea\PaymentPlanParamsResponse($message);
-//            } 
-//            elseif (property_exists($message, "DeliverOrderEuResult")) {
-//                $this->response = new Svea\DeliverOrderResult($message);
-//            } 
-//            elseif (property_exists($message, "CloseOrderEuResult")) {
-//                $this->response = new Svea\CloseOrderResult($message);
-//            }
-//            //webservice from hosted_admin
-//            elseif (property_exists($message, "message"))   {
-//                 $this->response = new Svea\HostedAdminResponse($message,$countryCode,$config);
-//            }
-//
-//        } 
-//        elseif ($message != NULL) {
-//            $this->response = new Svea\HostedPaymentResponse($message,$countryCode,$config);
-//        } 
-//        else {
-//            $this->response = "Response is not recognized.";
-//        }
-//    }    
-//    
-?>
+    public function test_handles_response_is_xml() {
+        $config = SveaConfig::getDefaultConfig();
+
+        $message = "string_that_pretends_to_be_an_encoded_xml_response";
+                
+        $response = new \SveaResponse( $message, "SE", $config );
+        $this->assertInstanceOf('Svea\HostedPaymentResponse', $response->getResponse() );
+        
+    }
+    
+    public function test_handles_response_is_SimpleXMLElement_object() {
+        $config = SveaConfig::getDefaultConfig();
+
+        $message = (object)array( "CloseOrderEuResult" => (object) array( "Accepted" => "1", "ResultCode" => "0" ) );
+        
+        $this->assertTrue( \is_object($message) ); 
+                     
+        $response = new \SveaResponse( $message, "SE", $config );
+        $this->assertInstanceOf('Svea\CloseOrderResult', $response->getResponse() );
+    }
+}
