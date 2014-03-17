f6a370238947ce5e55257de500679e8958ea7702
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-03-17 13:00:37 +0100

    test_annul_card_transaction_not_found passes

--- src/HostedRequests/HandleOrder/AnnulTransaction.php
+++ src/HostedRequests/HandleOrder/AnnulTransaction.php
@@ -60,34 +60,34 @@ class AnnulTransaction {
         return $request_fields;
     }
         
-//    /**
-//     * Do request using cURL
-//     * @return HostedAdminResponse
-//     */
-//    public function doRequest(){
-//        $fields = $this->prepareRequest();
-//        
-//        $fieldsString = "";
-//        foreach ($fields as $key => $value) {
-//            $fieldsString .= $key.'='.$value.'&';
-//        }
-//        rtrim($fieldsString, '&');
-//
-//        $ch = curl_init();
-//        curl_setopt($ch, CURLOPT_URL, $this->config->getEndpoint(SveaConfigurationProvider::HOSTED_ADMIN_TYPE). "credit");
-//        curl_setopt($ch, CURLOPT_POST, count($fields));
-//        curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldsString);
-//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//        //force curl to trust https
-//        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-//        //returns a html page with redirecting to bank...
-//        $responseXML = curl_exec($ch);
-//        curl_close($ch);
-//        
-//        // create SveaResponse to handle credit response
-//        $responseObj = new \SimpleXMLElement($responseXML);        
-//        $sveaResponse = new \SveaResponse($responseObj, $this->countryCode, $this->config);
-//
-//        return $sveaResponse->response; 
-//    }
+    /**
+     * Do request using cURL
+     * @return HostedAdminResponse
+     */
+    public function doRequest(){
+        $fields = $this->prepareRequest();
+        
+        $fieldsString = "";
+        foreach ($fields as $key => $value) {
+            $fieldsString .= $key.'='.$value.'&';
+        }
+        rtrim($fieldsString, '&');
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->config->getEndpoint(SveaConfigurationProvider::HOSTED_ADMIN_TYPE). "annul");
+        curl_setopt($ch, CURLOPT_POST, count($fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldsString);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        //force curl to trust https
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        //returns a html page with redirecting to bank...
+        $responseXML = curl_exec($ch);
+        curl_close($ch);
+        
+        // create SveaResponse to handle annul response
+        $responseObj = new \SimpleXMLElement($responseXML);        
+        $sveaResponse = new \SveaResponse($responseObj, $this->countryCode, $this->config);
+
+        return $sveaResponse->response; 
+    }
 }
