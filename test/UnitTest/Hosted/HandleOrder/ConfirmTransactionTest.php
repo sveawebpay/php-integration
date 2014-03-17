d3706fc7bf296f23c193a8d41aac554cf1c4329b
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-03-17 16:32:11 +0100

    added unit test ConfirmTransactionTest

---
+++ test/UnitTest/Hosted/HandleOrder/ConfirmTransactionTest.php
@@ -0,0 +1,106 @@
+<?php
+$root = realpath(dirname(__FILE__));
+
+require_once $root . '/../../../../src/Includes.php';
+require_once $root . '/../../../../src/WebServiceRequests/svea_soap/SveaSoapConfig.php';
+
+/**
+ * @author Kristian Grossman-Madsen for Svea Webpay
+ */
+class ConfirmTransactionTest extends PHPUnit_Framework_TestCase {
+        
+    protected $configObject;
+    protected $confirmObject;
+
+    // fixture, run once before each test method
+    protected function setUp() {
+        $this->configObject = Svea\SveaConfig::getDefaultConfig();
+        $this->confirmObject = WebPay::confirmTransaction( $this->configObject );
+    }
+
+    // test methods
+    function test_class_exists(){
+        
+        $this->assertInstanceOf( "Svea\ConfirmTransaction", $this->confirmObject);      
+    }
+//    
+//    function test_setCountryCode(){
+//        
+//        $countryCode = "SE";       
+//        $this->confirmObject->setCountryCode( $countryCode );
+//        
+//        //$this->assertEquals( $transactionId, $confirmObject->transactionId );    //oops, transactionId is private.
+//        $this->assertEquals( $countryCode, PHPUnit_Framework_Assert::readAttribute($this->confirmObject, 'countryCode') );
+//    }
+//    
+//    function test_setTransactionId( ){
+//        
+//        $transactionId = 987654;       
+//        $this->confirmObject->setTransactionId( $transactionId );
+//        
+//        //$this->assertEquals( $transactionId, $confirmObject->transactionId );    //oops, transactionId is private.
+//        $this->assertEquals( $transactionId, PHPUnit_Framework_Assert::readAttribute($this->confirmObject, 'transactionId') );
+//    }
+//    
+//    function test_setConfirmAmount() {
+//        
+//        $confirmAmount = 100;
+//        $this->confirmObject->setConfirmAmount( $confirmAmount );
+//        
+//        $this->assertEquals( $confirmAmount, PHPUnit_Framework_Assert::readAttribute($this->confirmObject, 'confirmAmount') );
+//    }
+//              
+//    function test_prepareRequest_array_contains_mac_merchantid_message() {
+//
+//        // set up confirmTransaction object & get request form
+//        $transactionId = 987654;       
+//        $this->confirmObject->setTransactionId( $transactionId );
+//
+//        $confirmAmount = 100;
+//        $this->confirmObject->setConfirmAmount( $confirmAmount );
+//        
+//        $countryCode = "SE";
+//        $this->confirmObject->setCountryCode($countryCode);
+//                
+//        $form = $this->confirmObject->prepareRequest();
+//
+//        // prepared request is message (base64 encoded), merchantid, mac
+//        $this->assertTrue( isset($form['merchantid']) );
+//        $this->assertTrue( isset($form['mac']) );
+//        $this->assertTrue( isset($form['message']) );
+//    }
+//    
+//    function test_prepareRequest_has_correct_merchantid_mac_and_confirm_request_message_contents() {
+//
+//        // set up confirmTransaction object & get request form
+//        $transactionId = 987654;       
+//        $this->confirmObject->setTransactionId( $transactionId );
+//
+//        $confirmAmount = 100;
+//        $this->confirmObject->setConfirmAmount( $confirmAmount );
+//        
+//        $countryCode = "SE";
+//        $this->confirmObject->setCountryCode($countryCode);
+//                
+//        $form = $this->confirmObject->prepareRequest();
+//        
+//        // get our merchantid & secret
+//        $merchantid = $this->configObject->getMerchantId( ConfigurationProvider::HOSTED_TYPE, $countryCode);
+//        $secret = $this->configObject->getSecret( ConfigurationProvider::HOSTED_TYPE, $countryCode);
+//         
+//        // check mechantid
+//        $this->assertEquals( $merchantid, urldecode($form['merchantid']) );
+//
+//        // check valid mac
+//        $this->assertEquals( hash("sha512", urldecode($form['message']). $secret), urldecode($form['mac']) );
+//        
+//        // check confirm request message contents
+//        $xmlMessage = new SimpleXMLElement( base64_decode(urldecode($form['message'])) );
+//
+//        $this->assertEquals( "confirm", $xmlMessage->getName() );   // root node        
+//        $this->assertEquals((string)$transactionId, $xmlMessage->transactionid);
+//        $this->assertEquals((string)$confirmAmount, $xmlMessage->amounttoconfirm);
+//        
+//    }
+}
+?>
