82d810ce98b1668564d0b933658ed47b40339091
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-03-18 08:58:19 +0100

    guiding test added in LowerTransactionIntegrationTest

--- test/IntegrationTest/HostedRequests/HandleOrder/LowerTransactionIntegrationTest.php
+++ test/IntegrationTest/HostedRequests/HandleOrder/LowerTransactionIntegrationTest.php
@@ -6,19 +6,19 @@ require_once $root . '/../../../../src/Includes.php';
 require_once $root . '/../../../TestUtil.php';
 
 /**
- * CreditTransactionIntegrationTest 
+ * LowerTransactionIntegrationTest 
  * 
  * @author Kristian Grossman-Madsen for Svea WebPay
  */
-class CreditTransactionIntegrationTest extends \PHPUnit_Framework_TestCase {
+class LowerTransactionIntegrationTest extends \PHPUnit_Framework_TestCase {
  
    /**
-     * test_card_creditTransaction_success creates an order using card payment, 
+     * test_lowerTransaction_card_success creates an order using card payment, 
      * pays using card & receives a transaction id, then credits the transaction
      * 
      * used as acceptance criteria/smoke test for credit transaction feature
      */
-    function _test_creditTransaction_card_success() { 
+    function test_lowerTransaction_card_success() { 
       
         // not yet implemented, requires webdriver support
 
@@ -48,23 +48,23 @@ class CreditTransactionIntegrationTest extends \PHPUnit_Framework_TestCase {
         
         // credit transcation using above the transaction transactionId
         
-        // assert response from creditTransaction equals success
+        // assert response from lowerTransactionAmount equals success
     }
     
     
     /**
-     * test_credit_card_transaction_not_found 
+     * test_lower_transaction_transaction_not_found 
      * 
      * used as initial acceptance criteria for credit transaction feature
      */  
-    function test_credit_card_transaction_not_found() {
+    function test_lower_transaction_transaction_not_found() {
              
         $transactionId = 987654;
         $amount = 100;
                 
-        $response = WebPay::creditTransaction( Svea\SveaConfig::getDefaultConfig() )
+        $response = WebPay::lowerTransaction( Svea\SveaConfig::getDefaultConfig() )
             ->setTransactionId( $transactionId )
-            ->setCreditAmount( $amount )
+            ->setAmountToLower( $amountToLower )
             ->setCountryCode( "SE" )
             ->doRequest();
 
@@ -76,35 +76,35 @@ class CreditTransactionIntegrationTest extends \PHPUnit_Framework_TestCase {
     }
     
     /**
-     * test_manual_credit_card 
+     * test_manual_lower_transaction_amount 
      * 
      * run this manually after you've performed a card transaction and have set
      * the transaction status to success using the tools in the logg admin.
      */  
-    function test_manual_credit_card() {
-        
-        // Stop here and mark this test as incomplete.
-        $this->markTestIncomplete(
-          'skeleton for manual test of credit card transaction'
-        );
-        
-        // Set the below to match the transaction, then run the test.
-        $customerrefno = 312;
-        $transactionId = 579893;
-        $amount = 100;
-                
-        $request = WebPay::creditTransaction( Svea\SveaConfig::getDefaultConfig() )
-            ->setTransactionId( $transactionId )
-            ->setCreditAmount( $amount )
-            ->setCountryCode( "SE" );
-    
-        $response = $request->doRequest();        
-        
-        $this->assertInstanceOf( "Svea\HostedAdminResponse", $response );
-        
-        // if we receive an error from the service, the integration test passes
-        $this->assertEquals( 1, $response->accepted );        
-        $this->assertEquals( $customerrefno, $response->customerrefno );  
-    }    
+//    function test_manual_lower_transaction_amount() {
+//        
+//        // Stop here and mark this test as incomplete.
+//        $this->markTestIncomplete(
+//          'skeleton for manual test of lower transaction amount'
+//        );
+//        
+//        // Set the below to match the transaction, then run the test.
+//        $customerrefno = 312;
+//        $transactionId = 579893;
+//        $amount = 100;
+//                
+//        $request = WebPay::lowerTransaction( Svea\SveaConfig::getDefaultConfig() )
+//            ->setTransactionId( $transactionId )
+//            ->setAmountToLower( $amountToLower )
+//            ->setCountryCode( "SE" );
+//    
+//        $response = $request->doRequest();        
+//        
+//        $this->assertInstanceOf( "Svea\HostedAdminResponse", $response );
+//        
+//        // if we receive an error from the service, the integration test passes
+//        $this->assertEquals( 1, $response->accepted );        
+//        $this->assertEquals( $customerrefno, $response->customerrefno );  
+//    }    
 }
 ?>
