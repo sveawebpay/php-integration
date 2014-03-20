a750e1573c4f83a98e3d16812824dfb73d7cd732
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-03-20 09:06:44 +0100

    updated HostedXmlBuilderTest

--- test/UnitTest/Hosted/Helper/HostedXmlBuilderTest.php
+++ test/UnitTest/Hosted/Helper/HostedXmlBuilderTest.php
@@ -31,31 +31,31 @@ class HostedXmlBuilderTest extends \PHPUnit_Framework_TestCase {
         
         $xmlBuilder = new HostedXmlBuilder();
         $xml = $xmlBuilder->getOrderXML($payment->calculateRequestValues(), $order);
-        
+       
         $this->assertEquals(1, substr_count($xml, "<payment>"));
         $this->assertEquals(1, substr_count($xml, "<customerrefno>1234</customerrefno>"));
-        $this->assertEquals(1, substr_count($xml, '<returnurl>http://myurl.se</returnurl>'));
-        $this->assertEquals(1, substr_count($xml, "<cancelurl/>"));
-        $this->assertEquals(1, substr_count($xml, "<amount>25000</amount>"));
         $this->assertEquals(1, substr_count($xml, "<currency>SEK</currency>"));
-        $this->assertEquals(1, substr_count($xml, "<lang/>"));
+        $this->assertEquals(1, substr_count($xml, "<amount>25000</amount>"));
+        $this->assertEquals(1, substr_count($xml, "<vat>5000</vat>"));
         $this->assertEquals(1, substr_count($xml, "<addinvoicefee>FALSE</addinvoicefee>"));
+        $this->assertEquals(1, substr_count($xml, "<lang>en</lang>"));
+        $this->assertEquals(1, substr_count($xml, '<returnurl>http://myurl.se</returnurl>'));
+        $this->assertEquals(1, substr_count($xml, "<cancelurl/>"));       
         $this->assertEquals(1, substr_count($xml, "<customer>"));
         $this->assertEquals(1, substr_count($xml, "<ssn>123456</ssn>"));
         $this->assertEquals(1, substr_count($xml, "<country>SE</country>"));
         $this->assertEquals(1, substr_count($xml, "</customer>"));
-        $this->assertEquals(1, substr_count($xml, "<vat>5000</vat>"));
+        $this->assertEquals(1, substr_count($xml, "<iscompany>FALSE</iscompany>"));
         $this->assertEquals(1, substr_count($xml, "<orderrows>"));
         $this->assertEquals(1, substr_count($xml, "<row>"));
-        $this->assertEquals(1, substr_count($xml, "<description></description>"));
-        $this->assertEquals(1, substr_count($xml, "<name></name>"));
         $this->assertEquals(1, substr_count($xml, "<sku></sku>"));
+        $this->assertEquals(1, substr_count($xml, "<name></name>"));
+        $this->assertEquals(1, substr_count($xml, "<description></description>"));
         $this->assertEquals(1, substr_count($xml, "<amount>12500</amount>"));
         $this->assertEquals(1, substr_count($xml, "<vat>2500</vat>"));
         $this->assertEquals(1, substr_count($xml, "<quantity>2</quantity>"));
         $this->assertEquals(1, substr_count($xml, "</row>"));
         $this->assertEquals(1, substr_count($xml, "</orderrows>"));
-        $this->assertEquals(1, substr_count($xml, "<iscompany>FALSE</iscompany>"));
         $this->assertEquals(1, substr_count($xml, "</payment>"));
     }
     
