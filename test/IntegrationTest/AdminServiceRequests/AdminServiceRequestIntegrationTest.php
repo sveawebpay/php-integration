9b367d52dd84fad0f73bd1e059a4f578780d253d
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-05-08 13:44:46 +0200

    Added integration test for admin service CancelOrderRequest.

---
+++ test/IntegrationTest/AdminServiceRequests/AdminServiceRequestIntegrationTest.php
@@ -0,0 +1,24 @@
+<?php
+
+$root = realpath(dirname(__FILE__));
+require_once $root . '/../../../src/Includes.php';
+
+/**
+ * @author Kristian Grossman-Madsen for Svea Webpay
+ */
+class AdminServiceRequestIntegrationTest extends PHPUnit_Framework_TestCase{
+
+    public function testCancelOrderRequest() {
+        
+        $sveaOrderIdToClose = 123456;
+        
+        $mockedBuilder = new StdClass();
+        $mockedBuilder->conf = $config;
+        $mockedBuilder->sveaOrderId = $sveaOrderIdToClose;
+        
+        $request = new Svea\CancelOrderRequest( $mockedBuilder );
+        $response = $request->doRequest();
+
+        $this->assertEquals(0, $response->ResultCode );    // raw response
+    }
+}
