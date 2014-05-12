29f347c4842f004453a456568e2533a1ca48414a
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-05-12 10:39:39 +0200

    CancelOrderRequest and CancelOrderBuilder classes finished for WebPayAdmin::cancelOrder entrypoint

--- src/AdminServiceRequests/AdminServiceRequest.php
+++ src/AdminServiceRequests/AdminServiceRequest.php
@@ -8,7 +8,46 @@ require_once SVEA_REQUEST_DIR . '/Includes.php';
  * 
  * @author Kristian Grossman-Madsen
  */
-class AdminServiceRequest {
+abstract class AdminServiceRequest {
+
+    /** @var string $action  the AdminService soap action called by this class */
+    protected $action; 
+
+    /** @var string $countryCode */
+    protected $countryCode; 
+          
+    /**
+     * Prepare and send request to Svea admin service using AdminSoap helpers
+     * @return StdClass  raw response @todo
+     */
+    public function doRequest() {
+        $soapRequest = $this->prepareRequest();
+        
+        $soapClient = new AdminSoap\SoapClient( $this->orderBuilder->conf->endpoint );
+        $response = $soapClient->doSoapCall($this->action, $soapRequest );
+        return $response;        
+    }
     
-    // TODO move common attributes, functions here
+    /**
+     * Validates the orderBuilder object to make sure that all required settings
+     * are present. If not, throws an exception. Actual validation is delegated
+     * to subclass validate() implementations.
+     *
+     * @throws ValidationException
+     */
+    public function validateRequest() {
+        // validate sub-class requirements by calling sub-class validate() method
+        $errors = $this->validate();
+        
+        if (count($errors) > 0) {
+            $exceptionString = "";
+            foreach ($errors as $key => $value) {
+                $exceptionString .="-". $key. " : ".$value."\n";
+            }
+
+            throw new ValidationException($exceptionString);
+        }    
+    }       
+
+    abstract function validate(); // validate is defined by subclasses, should validate all elements required for call is present
 }
--- test/UnitTest/AdminServiceRequests/CancelOrderRequestTest.php
+++ test/UnitTest/AdminServiceRequests/CancelOrderRequestTest.php
@@ -1,27 +1,47 @@
 <?php
+namespace Svea;
 
 $root = realpath(dirname(__FILE__));
 require_once $root . '/../../../src/Includes.php';
 
+$root = realpath(dirname(__FILE__));
+require_once $root . '/../../TestUtil.php';
+
 /**
  * @author Kristian Grossman-Madsen for Svea Webpay
  */
-class CancelOrderRequestTest extends PHPUnit_Framework_TestCase {
+class CancelOrderRequestTest extends \PHPUnit_Framework_TestCase {
 
+    public $builderObject;
+    
+    public function setUp() {        
+        $this->builderObject = new cancelOrderBuilder(SveaConfig::getDefaultConfig());  
+        $this->builderObject->setOrderId(123456);
+    }
+    
     public function testClassExists() {
-        $cancelOrderRequestObject = new Svea\CancelOrderRequest( new stdClass() );
+        $cancelOrderRequestObject = new CancelOrderRequest( new cancelOrderBuilder(SveaConfig::getDefaultConfig() ) );
         $this->assertInstanceOf('Svea\CancelOrderRequest', $cancelOrderRequestObject);
     }
     
-    public function test_prepareRequest_contains_Authentication() {
-        
-        $builderObject = new stdClass();
-        
-        $cancelOrderRequestObject = new Svea\CancelOrderRequest( $builderObject );
+    /**
+     * @expectedException Svea\ValidationException 
+     * @expectedExceptionMessage -missing value: sveaOrderId is required. Use function setSveaOrderId().
+     */
+    public function test_validate_throws_exception_on_missing_OrderId() {
+        unset( $this->builderObject->sveaOrderId );
+
+        $cancelOrderRequestObject = new CancelOrderRequest( $this->builderObject );
         $request = $cancelOrderRequestObject->prepareRequest();
-        
-        $this->assertTrue( isset($request->Authentication) );
-        $this->assertTrue( isset($request->Authentication->Username) );
-        $this->assertTrue( isset($request->Authentication->Password) );
-    }    
+    }
+    /**
+     * @expectedException Svea\ValidationException 
+     * @expectedExceptionMessage -missing value: orderType is required.
+     */
+    public function test_validate_throws_exception_on_missing_OrderType() {
+        unset( $this->builderObject->orderType );
+
+        $cancelOrderRequestObject = new CancelOrderRequest( $this->builderObject );
+        $request = $cancelOrderRequestObject->prepareRequest();
+    }      
 }
