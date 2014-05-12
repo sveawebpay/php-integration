bb703e4d917f43d39d66cbec16be5b8a50e5aae6
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-05-12 16:37:12 +0200

    added DeliverOrdersRequest class failing test

---
+++ src/AdminServiceRequests/DeliverOrdersRequest.php
@@ -0,0 +1,64 @@
+<?php
+namespace Svea;
+
+require_once SVEA_REQUEST_DIR . '/Includes.php';
+
+/**
+ * Admin Service DeliverOrdersRequest class
+ * 
+ * @author Kristian Grossman-Madsen
+ */
+class DeliverOrdersRequest extends AdminServiceRequest {
+    
+    /** @var cancelOrderBuilder $orderBuilder */
+    public $orderBuilder;    
+
+    /**
+     * @param cancelOrderBuilder $orderBuilder
+     */
+    public function __construct($deliverOrderBuilder) {
+        $this->action = "DeliverOrders";
+        $this->orderBuilder = $deliverOrderBuilder;
+    }    
+
+    /**
+     * populate and return soap request contents
+     * @return Svea\AdminSoap\CancelOrderRequest
+     */    
+    public function prepareRequest() {        
+        
+        $this->validateRequest();
+        
+//        $soapRequest = new AdminSoap\CancelOrderRequest( 
+//                new AdminSoap\Authentication( 
+//                    $this->orderBuilder->conf->username, 
+//                    $this->orderBuilder->conf->password 
+//                ),
+//                $this->orderBuilder->sveaOrderId, 
+//                $this->orderBuilder->orderType,
+//                $this->orderBuilder->conf->clientId 
+//        );
+        
+        return $soapRequest;
+    }
+        
+//    public function validate() {
+//        $errors = array();
+//        $errors = $this->validateOrderId($errors);
+//        $errors = $this->validateOrderType($errors);
+//        return $errors;
+//    }
+//    
+//    private function validateOrderId($errors) {
+//        if (isset($this->orderBuilder->sveaOrderId) == FALSE) {                                                        
+//            $errors['missing value'] = "sveaOrderId is required.";
+//        }
+//        return $errors;
+//    }               
+//    private function validateOrderType($errors) {
+//        if (isset($this->orderBuilder->orderType) == FALSE) {                                                        
+//            $errors['missing value'] = "orderType is required.";
+//        }
+//        return $errors;
+//    }                     
+}        
---
+++ test/UnitTest/AdminServiceRequests/DeliverOrdersRequestTest.php
@@ -0,0 +1,47 @@
+<?php
+namespace Svea;
+
+$root = realpath(dirname(__FILE__));
+require_once $root . '/../../../src/Includes.php';
+
+$root = realpath(dirname(__FILE__));
+require_once $root . '/../../TestUtil.php';
+
+/**
+ * @author Kristian Grossman-Madsen for Svea Webpay
+ */
+class DeliverOrdersRequestTest extends \PHPUnit_Framework_TestCase {
+
+    public $builderObject;
+    
+    public function setUp() {        
+        $this->builderObject = new deliverOrdersBuilder(SveaConfig::getDefaultConfig());  
+//        $this->builderObject->setOrderId(123456);
+    }
+    
+    public function testClassExists() {
+        $deliverOrdersRequestObject = new DeliverOrdersRequest( new deliverOrdersBuilder(SveaConfig::getDefaultConfig() ) );
+        $this->assertInstanceOf('Svea\DeliverOrdersRequest', $deliverOrdersRequestObject);
+    }
+    
+    /**
+     * @expectedException Svea\ValidationException 
+     * @expectedExceptionMessage -missing value: sveaOrderId is required. Use function setSveaOrderId().
+     */
+//    public function test_validate_throws_exception_on_missing_OrderId() {
+//        unset( $this->builderObject->sveaOrderId );
+//
+//        $cancelOrderRequestObject = new CancelOrderRequest( $this->builderObject );
+//        $request = $cancelOrderRequestObject->prepareRequest();
+//    }
+    /**
+     * @expectedException Svea\ValidationException 
+     * @expectedExceptionMessage -missing value: orderType is required.
+     */
+//    public function test_validate_throws_exception_on_missing_OrderType() {
+//        unset( $this->builderObject->orderType );
+//
+//        $cancelOrderRequestObject = new CancelOrderRequest( $this->builderObject );
+//        $request = $cancelOrderRequestObject->prepareRequest();
+//    }      
+}
