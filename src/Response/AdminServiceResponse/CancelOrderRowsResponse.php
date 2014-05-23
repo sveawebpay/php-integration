abb0ff2ce924436bf14286d4a6bbf511e65f3b2a
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-05-23 16:17:11 +0200

    added AdminServiceResponse as parent class for admin service request responses

--- src/Response/AdminServiceResponse/CancelOrderRowsResponse.php
+++ src/Response/AdminServiceResponse/CancelOrderRowsResponse.php
@@ -4,11 +4,11 @@ namespace Svea;
 require_once 'AdminServiceResponse.php';
 
 /**
- * Handles the Svea Admin Web Service CancelOrder request response.
+ * Handles the Svea Admin Web Service CancelOrderRowsResponse request response.
  * 
  * @author Kristian Grossman-Madsen
  */
-class CancelOrderResponse extends AdminServiceResponse {
+class CancelOrderRowsResponse extends AdminServiceResponse {
   
     function __construct($message) {
         $this->formatObject($message);  
