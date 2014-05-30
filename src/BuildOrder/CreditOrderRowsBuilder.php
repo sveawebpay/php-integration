7e4d70fae9c10356919a242de1cec3839b327d38
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-05-30 14:12:18 +0200

    CreditOrderRowsRequest in place

--- src/BuildOrder/CreditOrderRowsBuilder.php
+++ src/BuildOrder/CreditOrderRowsBuilder.php
@@ -34,22 +34,11 @@ class CreditOrderRowsBuilder {
     public $orderRows;
 
     public function __construct($config) {
-         $this->conf = $config;
-         $this->orderRows = array();
+        $this->conf = $config;
+        $this->rowsToCredit = array();         
+        $this->creditOrderRows = array();
     }
 
-    /**
-     * Required. Use SveaOrderId recieved with createOrder response.
-     * @param string $orderIdAsString
-     * @return $this
-     */
-    public function setOrderId($orderIdAsString) {
-        $this->orderId = $orderIdAsString;
-        return $this;
-    }
-    /** string $orderId  Svea order id to query, as returned in the createOrder request response, either a transactionId or a SveaOrderId */
-    public $orderId;
-    
     /**
      * Required. Use same countryCode as in createOrder request.
      * @param string $countryCode
@@ -75,12 +64,68 @@ class CreditOrderRowsBuilder {
     public $orderType;    
 
     /**
+     * Required. Use InvoiceId recieved with deliverOrder response.
+     * @param numeric $invoiceIdAsString
+     * @return $this
+     */
+    public function setInvoiceId($invoiceIdAsString) {
+        $this->invoiceId = $invoiceIdAsString;
+        return $this;
+    }
+    /** numeric $orderId  Svea order id to query, as returned in the createOrder request response, either a transactionId or a SveaOrderId */
+    public $invoiceId;
+   
+    /**
+     * Invoice payments only! Required.
+     * @param string DistributionType $distributionTypeAsConst  i.e. DistributionType::POST|DistributionType::EMAIL
+     * @return $this
+     */
+    public function setInvoiceDistributionType($distributionTypeAsConst) {
+        if ($distributionTypeAsConst != \DistributionType::EMAIL || $distributionTypeAsConst != \DistributionType::POST) {
+            $distributionTypeAsConst = trim($distributionTypeAsConst);
+            if (preg_match("/post/i", $distributionTypeAsConst)) {
+                $distributionTypeAsConst = \DistributionType::POST;
+            } elseif (preg_match("/mail/i", $distributionTypeAsConst)) {
+                $distributionTypeAsConst = \DistributionType::EMAIL;
+            } else {
+                $distributionTypeAsConst = \DistributionType::POST;
+            }
+        }
+        $this->distributionType = $distributionTypeAsConst;
+        return $this;
+    }
+    /**
+     * @var string  "Post" or "Email"
+     */
+    public $distributionType;    
+        
+    /**
+     * Required.
+     * @param numeric $rowNumber
+     * @return $this
+     */
+    public function setRowToCredit( $rowNumber ) {
+        $this->rowsToCredit[] = $rowNumber;
+        return $this;
+    }    
+    
+    /**
+     * Convenience method to provide several row numbers at once.
+     * @param int[] $rowNumbers
+     * @return $this
+     */
+    public function setRowsToCredit( $rowNumbers ) {
+        array_merge( $this->rowsToCredit, $rowNumbers );
+        return $this;
+    } 
+       
+    /**
      * Required.
      * @param OrderRow $row
      * @return $this
      */
-    public function creditOrderRow( $row ) {
-        $this->orderRows[] = $row;
+    public function setCreditOrderRow( $row ) {
+        $this->creditOrderRows[] = $row;
         return $this;
     }    
     
@@ -89,8 +134,8 @@ class CreditOrderRowsBuilder {
      * @param OrderRow[] $rows
      * @return $this
      */
-    public function creditOrderRows( $rows ) {
-        array_merge( $this->orderRows, $rows );
+    public function setCreditOrderRows( $rows ) {
+        array_merge( $this->creditOrderRows, $rows );
         return $this;
     }    
 
