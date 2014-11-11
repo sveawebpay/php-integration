dac03ad4a34ab7e26f795c07fc7beca521e4c9cb
Anneli Halldn <anneli.halldn@sveaekonomi.se>
2014-11-11 13:23:40 +0100

    changed doc about setCountryCode

--- src/BuildOrder/DeliverOrderBuilder.php
+++ src/BuildOrder/DeliverOrderBuilder.php
@@ -1,38 +1,38 @@
 <?php
 namespace Svea;
 
-require_once 'OrderBuilder.php'; 
+require_once 'OrderBuilder.php';
 require_once SVEA_REQUEST_DIR . '/Includes.php';
 
 /**
- * DeliverOrderBuilder collects and prepares order data for use in a deliver 
+ * DeliverOrderBuilder collects and prepares order data for use in a deliver
  * order request to Svea.
- * 
- * Use setOrderId() to specify the Svea order id, this is the order id returned 
- * with the original create order request response. For card orders, you can 
+ *
+ * Use setOrderId() to specify the Svea order id, this is the order id returned
+ * with the original create order request response. For card orders, you can
  * optionally use setTransactionId() instead.
  *
  * Use setCountryCode() to specify the country code matching the original create
  * order request.
- * 
- * Use setInvoiceDistributionType() with the DistributionType matching how your  
- * account is configured to send out invoices. (Please contact Svea if unsure.) 
+ *
+ * Use setInvoiceDistributionType() with the DistributionType matching how your
+ * account is configured to send out invoices. (Please contact Svea if unsure.)
  *
  * Use setNumberOfCreditDays() to specify the number of credit days for an invoice.
  *
  * (Deprecated -- to partially deliver an invoice order, you can specify order rows to deliver
  * using the addOrderRows() method. Use the WebPayAdmin::deliverOrderRows entrypoint instead.)
- * 
- * (Deprecated -- to issue a credit invoice, you can specify credit order rows to deliver using 
+ *
+ * (Deprecated -- to issue a credit invoice, you can specify credit order rows to deliver using
  * setCreditInvoice() and addOrderRows(). Use the WebPayAdmin::creditOrderRow entrypoint instead.)
- * 
+ *
  * To deliver an invoice, partpayment or card order in full, use the WebPay::deliverOrder
  * entrypoint without specifying order rows.
- * 
+ *
  * When specifying orderrows, WebPay::deliverOrder is used in a similar way to WebPay::createOrder
- * and makes use of the same order item information. Add order rows that you want delivered and send 
- * the request, specified rows will automatically be matched to the rows sent when creating the order. 
- * 
+ * and makes use of the same order item information. Add order rows that you want delivered and send
+ * the request, specified rows will automatically be matched to the rows sent when creating the order.
+ *
  * We recommend storing the createOrder orderRow objects to ensure that deliverOrder order rows match.
  * If an order row that was present in the createOrder request is not present in from the deliverOrder
  * request, the order will be partially delivered, and any left out items will not be invoiced by Svea.
@@ -43,21 +43,21 @@ require_once SVEA_REQUEST_DIR . '/Includes.php';
 class DeliverOrderBuilder extends OrderBuilder {
 
     /** @var string $orderId  order id/transaction id as returned in the createOrder request response,  */
-    public $orderId;   
+    public $orderId;
 
     /** @var string $distributionType -- one of DistributionType::POST, ::EMAIL */
     public $distributionType;
 
     /** @var string $captureDate -- confirmation date on format "YYYY-MM-DD" */
-    public $captureDate;   
-    
+    public $captureDate;
+
     /**
      * @deprecated 2.0.0 -- use WebPayAdmin::updateOrderRows in order to modify an existing order.
      *
-     * 2.x: Optional. Use setOrderRows to add invoice order rows to deliver. 
-     * Rows matching the original create order request order rows will be 
-     * invoiced by Svea. If not all order rows match, an invoice order will be 
-     * partially delivered/invoiced, see the Svea Web Service EU API 
+     * 2.x: Optional. Use setOrderRows to add invoice order rows to deliver.
+     * Rows matching the original create order request order rows will be
+     * invoiced by Svea. If not all order rows match, an invoice order will be
+     * partially delivered/invoiced, see the Svea Web Service EU API
      * documentation for details.
      */
     public function addOrderRow($itemOrderRowObject) {
@@ -81,20 +81,20 @@ class DeliverOrderBuilder extends OrderBuilder {
      */
     public function setTransactionId($transactionIdAsString) {
         return $this->setOrderId($transactionIdAsString);
-    }     
+    }
 
     /**
      * Optional for card orders -- confirmation date on format "YYYY-MM-DD"
-     * 
+     *
      * If no date is given the current date is used per default.
-     * 
+     *
      * @param string $orderIdAsString
      * @return $this
      */
     public function setCaptureDate($captureDateAsString) {
         $this->captureDate = $captureDateAsString;
         return $this;
-    }        
+    }
 
     /**
      * Invoice only, required.
@@ -105,7 +105,7 @@ class DeliverOrderBuilder extends OrderBuilder {
         $this->distributionType = $distributionTypeAsConst;
         return $this;
     }
- 
+
     /**
      * Invoice only, optional
      * Use if this should be a credit invoice
@@ -115,7 +115,7 @@ class DeliverOrderBuilder extends OrderBuilder {
     public function setCreditInvoice($invoiceId) {
         $this->invoiceIdToCredit = $invoiceId;
         return $this;
-    }  
+    }
     /**
      * If Invoice is to be credit Invoice
      * @var Invoice Id
@@ -134,15 +134,15 @@ class DeliverOrderBuilder extends OrderBuilder {
     /** @var int $numberOfCreditDays */
     public $numberOfCreditDays;
 
-    
+
     /**
-     * To ensure backwards compatibility, deliverInvoiceOrder() checks if the 
-     * order has any order rows defined, and if so performs a DeliverOrderEU 
+     * To ensure backwards compatibility, deliverInvoiceOrder() checks if the
+     * order has any order rows defined, and if so performs a DeliverOrderEU
      * request to Svea, passing on the order rows.
-     * 
-     * If no order rows are defined, deliverInvoiceOrder() performs a 
+     *
+     * If no order rows are defined, deliverInvoiceOrder() performs a
      * DeliverOrders request using the Admin Web Service API at Svea.
-     * 
+     *
      * @return WebService\DeliverInvoice|AdminService\DeliverOrdersRequest
      */
     public function deliverInvoiceOrder() {
@@ -169,33 +169,33 @@ class DeliverOrderBuilder extends OrderBuilder {
 
     /**
      * deliverCardOrder() sets the status of a card order to CONFIRMED.
-     * 
-     * A default capturedate equal to the current date will be supplied. This 
-     * may be overridden using the ConfirmTransaction setCaptureDate() method 
+     *
+     * A default capturedate equal to the current date will be supplied. This
+     * may be overridden using the ConfirmTransaction setCaptureDate() method
      * on the returned ConfirmTransaction object.
-     * 
+     *
      * @return DeliverPaymentPlan
      */
-    public function deliverCardOrder() {        
+    public function deliverCardOrder() {
         $this->orderType = \ConfigurationProvider::HOSTED_TYPE;
-        
+
         // validation is done in ConfirmTransaction
 
         // if no captureDate set, use today's date as default.
         if( !isset( $this->captureDate ) ) {
             $defaultCaptureDate = explode("T", date('c')); // [0] contains date part
-            $this->setCaptureDate = $defaultCaptureDate[0];
+            $this->captureDate = $defaultCaptureDate[0];
         }
-        
+
         $confirmTransaction = new HostedService\ConfirmTransaction($this->conf);
         $confirmTransaction->transactionId = $this->orderId;
         $confirmTransaction->captureDate = $this->captureDate;
         $confirmTransaction->countryCode = $this->countryCode;
         return $confirmTransaction;
-    }    
+    }
 
-    /**  
-     * @param \ConfigurationProvider $config 
+    /**
+     * @param \ConfigurationProvider $config
      */
     public function __construct($config) {
         parent::__construct($config);
