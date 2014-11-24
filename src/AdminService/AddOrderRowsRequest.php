f61b94c09f11526d057d6ed4789bb759504062a7
Anneli Halldn <anneli.halldn@sveaekonomi.se>
2014-11-24 10:32:44 +0100

    empty node error fix in xml

--- src/AdminService/AddOrderRowsRequest.php
+++ src/AdminService/AddOrderRowsRequest.php
@@ -9,11 +9,11 @@ require_once 'AdminServiceRequest.php';
 
 /**
  * Admin Service AddOrderRowsRequest class
- * 
+ *
  * @author Kristian Grossman-Madsen
  */
 class AddOrderRowsRequest extends AdminServiceRequest {
-    
+
     /** @var AddOrderRowBuilder $orderBuilder */
     public $orderBuilder;
 
@@ -29,15 +29,15 @@ class AddOrderRowsRequest extends AdminServiceRequest {
      * populate and return soap request contents using AdminSoap helper classes to get the correct data format
      * @return \Svea\AdminService\AdminSoap\AddOrderRowsRequest
      * @throws \Svea\ValidationException
-     */    
-    public function prepareRequest() {        
-                   
+     */
+    public function prepareRequest() {
+
         $this->validateRequest();
 
-        $orderRowNumbers = array();        
-        foreach( $this->orderBuilder->orderRows as $orderRow ) {      
+        $orderRowNumbers = array();
+        foreach( $this->orderBuilder->orderRows as $orderRow ) {
 
-            // handle different ways to spec an orderrow            
+            // handle different ways to spec an orderrow
             // inc + ex
             if( !isset($orderRow->vatPercent) && (isset($orderRow->amountExVat) && isset($orderRow->amountIncVat)) ) {
                 $orderRow->vatPercent = \Svea\WebService\WebServiceRowFormatter::calculateVatPercentFromPriceExVatAndPriceIncVat($orderRow->amountIncVat, $orderRow->amountExVat );
@@ -47,67 +47,72 @@ class AddOrderRowsRequest extends AdminServiceRequest {
                 $orderRow->amountExVat = \Svea\WebService\WebServiceRowFormatter::convertIncVatToExVat($orderRow->amountIncVat, $orderRow->vatPercent);
             }
             // % + ex, no need to do anything
-                              
-            $orderRows[] = new \SoapVar( 
+
+            //discountPercent must be 0 or an int
+            if(!isset($orderRow->discountPercent)) {
+                $orderRow->discountPercent = 0;
+            }
+
+            $orderRows[] = new \SoapVar(
                 new AdminSoap\OrderRow(
-                    $orderRow->articleNumber, 
+                    $orderRow->articleNumber,
                     $orderRow->name.": ".$orderRow->description,
                     $orderRow->discountPercent,
-                    $orderRow->quantity, 
-                    $orderRow->amountExVat, 
-                    $orderRow->unit, 
+                    $orderRow->quantity,
+                    $orderRow->amountExVat,
+                    $orderRow->unit,
                     $orderRow->vatPercent
                 ),
-                SOAP_ENC_OBJECT, null, null, 'OrderRow', "http://schemas.datacontract.org/2004/07/DataObjects.Webservice" 
+                SOAP_ENC_OBJECT, null, null, 'OrderRow', "http://schemas.datacontract.org/2004/07/DataObjects.Webservice"
             );
         }
-        
-        $soapRequest = new AdminSoap\AddOrderRowsRequest( 
-            new AdminSoap\Authentication( 
-                $this->orderBuilder->conf->getUsername( ($this->orderBuilder->orderType), $this->orderBuilder->countryCode ), 
-                $this->orderBuilder->conf->getPassword( ($this->orderBuilder->orderType), $this->orderBuilder->countryCode ) 
+
+        $soapRequest = new AdminSoap\AddOrderRowsRequest(
+            new AdminSoap\Authentication(
+                $this->orderBuilder->conf->getUsername( ($this->orderBuilder->orderType), $this->orderBuilder->countryCode ),
+                $this->orderBuilder->conf->getPassword( ($this->orderBuilder->orderType), $this->orderBuilder->countryCode )
             ),
             $this->orderBuilder->conf->getClientNumber( ($this->orderBuilder->orderType), $this->orderBuilder->countryCode ),
-            new \SoapVar($orderRows, SOAP_ENC_OBJECT),            
+            new \SoapVar($orderRows, SOAP_ENC_OBJECT),
             AdminServiceRequest::CamelCaseOrderType( $this->orderBuilder->orderType ),
             $this->orderBuilder->orderId
         );
         return $soapRequest;
     }
-        
+
     public function validate() {
         $errors = array();
         $errors = $this->validateOrderId($errors);
         $errors = $this->validateOrderType($errors);
         $errors = $this->validateCountryCode($errors);
-        $errors = $this->validateRowsToAdd($errors);     
+        $errors = $this->validateRowsToAdd($errors);
         $errors = $this->validateRowsHasPriceAndVatInformation($errors);
         return $errors;
     }
-    
+
     private function validateOrderId($errors) {
-        if (isset($this->orderBuilder->orderId) == FALSE) {                                                        
+        if (isset($this->orderBuilder->orderId) == FALSE) {
             $errors[] = array('missing value' => "orderId is required.");
         }
         return $errors;
-    }               
+    }
 
     private function validateOrderType($errors) {
-        if (isset($this->orderBuilder->orderType) == FALSE) {                                                        
+        if (isset($this->orderBuilder->orderType) == FALSE) {
             $errors[] = array('missing value' => "orderType is required.");
         }
         return $errors;
-    }            
-    
+    }
+
     private function validateCountryCode($errors) {
-        if (isset($this->orderBuilder->countryCode) == FALSE) {                                                        
+        if (isset($this->orderBuilder->countryCode) == FALSE) {
             $errors[] = array('missing value' => "countryCode is required.");
         }
         return $errors;
-    }    
-    
+    }
+
     private function validateRowsToAdd($errors) {
-        if (isset($this->orderBuilder->orderRows) == FALSE) {                                                        
+        if (isset($this->orderBuilder->orderRows) == FALSE) {
             $errors[] = array('missing value' => "orderRows is required.");
         }
         return $errors;
@@ -115,12 +120,12 @@ class AddOrderRowsRequest extends AdminServiceRequest {
 
     private function validateRowsHasPriceAndVatInformation($errors) {
         if( isset($this->orderBuilder->orderRows) ) {
-            foreach( $this->orderBuilder->orderRows as $orderRow ) {                                                        
-                if( !isset($orderRow->vatPercent) && (!isset($orderRow->amountIncVat) && !isset($orderRow->amountExVat)) ) {            
+            foreach( $this->orderBuilder->orderRows as $orderRow ) {
+                if( !isset($orderRow->vatPercent) && (!isset($orderRow->amountIncVat) && !isset($orderRow->amountExVat)) ) {
                     $errors[] = array('missing order row vat information' => "cannot calculate orderRow vatPercent, need at least two of amountExVat, amountIncVat and vatPercent.");
                 }
             }
         }
         return $errors;
     }
-}        
+}
--- src/AdminService/admin_soap/SoapClient.php
+++ src/AdminService/admin_soap/SoapClient.php
@@ -4,7 +4,7 @@ namespace Svea\AdminService\AdminSoap;
 class SoapClient {
 
     private $client;
-    
+
     /**
      * Constructor, sets up soap server and SoapClient
      * @param string $endpoint
@@ -12,7 +12,7 @@ class SoapClient {
     public function __construct($endpoint) {
         $this->client = $this->setSoapClient( $endpoint );
     }
-    
+
     /**
      * When used from PHP, the SveaWebPay Administration Service requires some configuration.
      * getSoapClient() takes the endpoint url and returns a SoapClient with a working set
@@ -22,55 +22,55 @@ class SoapClient {
      * @return SoapClient
      */
     public function setSoapClient( $endpoint ) {
-        $client = new \SoapClient( 
+        $client = new \SoapClient(
             null,
             array(
                 'location' => $endpoint,
                 'uri' => "http://tempuri.org/",
-                'use' => SOAP_LITERAL,    
+                'use' => SOAP_LITERAL,
                 'exceptions'=> 1,
                 'connection_timeout' => 60,
                 'trace' => 1,
                 'soap_version' => SOAP_1_1
             )
-        );  
-        
+        );
+
         return $client;
     }
-    
+
     /**
      * doSoapCall takes the $action name of the soap function you wish to call,
-     * and the $request data. 
-     * 
+     * and the $request data.
+     *
      * Use the provided AdminSoap classes to build the request content, as these
      * perform the \SoapVar conversion. Note that doSoapCall performs the final
-     * wrapping of the request contents, so you do not need a "request" SoapVar. 
-     * 
+     * wrapping of the request contents, so you do not need a "request" SoapVar.
+     *
      * @param string $action
      * @param \SoapVar $request
      * @return StdClass $response
      */
     public function doSoapCall($action, $request) {
-        //try { // debug
+        try { // debug
 
         // wrap the request
         $wrappedRequest = new \SoapVar( $request, SOAP_ENC_OBJECT, "-", "--", "request", "http://tempuri.org/");
 
         // do soapcall
-        $response = $this->client->__soapCall( $action, array( $wrappedRequest ), 
+        $response = $this->client->__soapCall( $action, array( $wrappedRequest ),
             array( "soapaction" => 'http://tempuri.org/IAdminService/'.$action )
         );
 
-        //} //debug
-        //catch( \SoapFault $e ) {
-        //    echo "AdminService SoapFault Exception: ";
-        //    echo $this->client->__getLastRequest() . "\n";
-        //    echo $this->client->__getLastRequestHeaders();
-        //    die;
-        //}; 
-        //echo "Debugging AdminService SoapCall: ";
-        //echo $this->client->__getLastRequest() . "\n";
-        //echo $this->client->__getLastRequestHeaders();             
+        } //debug
+        catch( \SoapFault $e ) {
+            echo "AdminService SoapFault Exception: ";
+            echo $this->client->__getLastRequest() . "\n";
+            echo $this->client->__getLastRequestHeaders();
+            die;
+        };
+        echo "Debugging AdminService SoapCall: ";
+        echo $this->client->__getLastRequest() . "\n";
+        echo $this->client->__getLastRequestHeaders();
         return $response;
     }
 }
