9b33c1451345c6c3a8bfe92db3b1a50a7c4bf186
Anneli Halldn <anneli.halldn@sveaekonomi.se>
2014-12-04 15:00:50 +0100

    INTG-562 updateOrderrows rounding case mixed orderrows

--- test/UnitTest/AdminService/UpdateOrderRowsRequestTest.php
+++ test/UnitTest/AdminService/UpdateOrderRowsRequestTest.php
@@ -62,4 +62,87 @@ class AddOrderRowsRequestTest extends \PHPUnit_Framework_TestCase {
          $this->assertEquals(123.9876, $request->UpdatedOrderRows->enc_value->enc_value[0]->enc_value->PricePerUnit->enc_value);
         $this->assertTrue($request->UpdatedOrderRows->enc_value->enc_value[0]->enc_value->PriceIncludingVat->enc_value);
     }
+    public function test_add_single_orderRow_mixed_types_1() {
+        $config = Svea\SveaConfig::getDefaultConfig();
+        $request = WebPayAdmin::updateOrderRows($config)
+              ->setCountryCode('SE')
+              ->setOrderId('test')
+              ->updateOrderRow(    WebPayItem::numberedOrderRow()
+                        ->setRowNumber(1)
+                        ->setAmountExVat(99.99)
+                        ->setVatPercent(24)
+                        ->setQuantity(1)
+                   )
+              ->updateOrderRow(    WebPayItem::numberedOrderRow()
+                        ->setRowNumber(1)
+                        ->setAmountIncVat(123.9876)
+                        ->setVatPercent(24)
+                        ->setQuantity(1)
+                   )
+              ->updateInvoiceOrderRows()
+              ->prepareRequest();
+//      print_r($request);
+         $this->assertEquals(99.99, $request->UpdatedOrderRows->enc_value->enc_value[0]->enc_value->PricePerUnit->enc_value);
+        $this->assertFalse($request->UpdatedOrderRows->enc_value->enc_value[0]->enc_value->PriceIncludingVat->enc_value);
+         $this->assertEquals(99.99, $request->UpdatedOrderRows->enc_value->enc_value[1]->enc_value->PricePerUnit->enc_value);
+        $this->assertFalse($request->UpdatedOrderRows->enc_value->enc_value[1]->enc_value->PriceIncludingVat->enc_value);
+    }
+    public function test_add_single_orderRow_mixed_types_2() {
+        $config = Svea\SveaConfig::getDefaultConfig();
+        $request = WebPayAdmin::updateOrderRows($config)
+              ->setCountryCode('SE')
+              ->setOrderId('test')
+              ->updateOrderRow(    WebPayItem::numberedOrderRow()
+                        ->setRowNumber(1)
+                        ->setAmountExVat(99.99)
+                        ->setAmountIncVat(123.9876)
+                        ->setQuantity(1)
+                   )
+              ->updateOrderRow(    WebPayItem::numberedOrderRow()
+                        ->setRowNumber(1)
+                        ->setAmountIncVat(123.9876)
+                        ->setVatPercent(24)
+                        ->setQuantity(1)
+                   )
+              ->updateInvoiceOrderRows()
+              ->prepareRequest();
+//      print_r($request);
+         $this->assertEquals(99.99, $request->UpdatedOrderRows->enc_value->enc_value[0]->enc_value->PricePerUnit->enc_value);
+        $this->assertFalse($request->UpdatedOrderRows->enc_value->enc_value[0]->enc_value->PriceIncludingVat->enc_value);
+         $this->assertEquals(99.99, $request->UpdatedOrderRows->enc_value->enc_value[1]->enc_value->PricePerUnit->enc_value);
+        $this->assertFalse($request->UpdatedOrderRows->enc_value->enc_value[1]->enc_value->PriceIncludingVat->enc_value);
+    }
+    public function test_add_single_orderRow_mixed_types_3() {
+        $config = Svea\SveaConfig::getDefaultConfig();
+        $request = WebPayAdmin::updateOrderRows($config)
+              ->setCountryCode('SE')
+              ->setOrderId('test')
+              ->updateOrderRow(    WebPayItem::numberedOrderRow()
+                        ->setRowNumber(1)
+                        ->setAmountExVat(99.99)
+                        ->setAmountIncVat(123.9876)
+                        ->setQuantity(1)
+                   )
+              ->updateOrderRow(    WebPayItem::numberedOrderRow()
+                        ->setRowNumber(1)
+                        ->setAmountIncVat(123.9876)
+                        ->setVatPercent(24)
+                        ->setQuantity(1)
+                   )
+              ->updateOrderRow(    WebPayItem::numberedOrderRow()
+                        ->setRowNumber(1)
+                        ->setAmountExVat(99.99)
+                        ->setVatPercent(24)
+                        ->setQuantity(1)
+                   )
+              ->updateInvoiceOrderRows()
+              ->prepareRequest();
+//      print_r($request);
+         $this->assertEquals(99.99, $request->UpdatedOrderRows->enc_value->enc_value[0]->enc_value->PricePerUnit->enc_value);
+        $this->assertFalse($request->UpdatedOrderRows->enc_value->enc_value[0]->enc_value->PriceIncludingVat->enc_value);
+         $this->assertEquals(99.99, $request->UpdatedOrderRows->enc_value->enc_value[1]->enc_value->PricePerUnit->enc_value);
+        $this->assertFalse($request->UpdatedOrderRows->enc_value->enc_value[1]->enc_value->PriceIncludingVat->enc_value);
+         $this->assertEquals(99.99, $request->UpdatedOrderRows->enc_value->enc_value[2]->enc_value->PricePerUnit->enc_value);
+        $this->assertFalse($request->UpdatedOrderRows->enc_value->enc_value[2]->enc_value->PriceIncludingVat->enc_value);
+    }
 }
