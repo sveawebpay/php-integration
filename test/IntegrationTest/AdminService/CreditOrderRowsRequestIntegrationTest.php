65b24aab60914fdab3c56df242264cf71830e08d
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2015-05-05 09:30:19 +0200

    moved tests to CreditOrderRowsRequestIntegrationTest.php

--- test/IntegrationTest/AdminService/CreditOrderRowsRequestIntegrationTest.php
+++ test/IntegrationTest/AdminService/CreditOrderRowsRequestIntegrationTest.php
@@ -241,6 +241,71 @@ class CreditOrderRowsRequestIntegrationTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals("123.99", $query2->numberedOrderRows[0]->amountIncVat);   // sent 99.99 ex * 1.24 => sent 123.9876 inc => 123.99 queried
         $this->assertEquals("24", $query2->numberedOrderRows[0]->vatPercent);
         //print_r($orderResponse->sveaOrderId);
-    }      
+    }   
+    
+    
+   
+    /// characterizing tests for INTG-551
+    function test_creditOrderRows_handles_creditOrderRows_specified_using_exvat_and_vatpercent() {       
+        // needs either setRow(s)ToCredit or addCreditOrderRow(s)    
+        $creditOrder = WebPayAdmin::creditOrderRows( Svea\SveaConfig::getDefaultConfig() )
+            ->setInvoiceId("123456789")                
+            ->setInvoiceDistributionType(DistributionType::POST)
+            ->setCountryCode("SE")            
+            ->addCreditOrderRow( 
+                    WebPayItem::orderRow()
+                        ->setAmountExVat(10.00)
+                        ->setVatPercent(25)
+                        ->setQuantity(1)
+            )
+        ;
+        $request = $creditOrder->creditInvoiceOrderRows()->prepareRequest();
+     
+        $this->assertEquals("10", $request->NewCreditInvoiceRows->enc_value[0]->enc_value->PricePerUnit->enc_value);
+        $this->assertEquals("25", $request->NewCreditInvoiceRows->enc_value[0]->enc_value->VatPercent->enc_value);
+        $this->assertEquals(null, $request->NewCreditInvoiceRows->enc_value[0]->enc_value->PriceIncludingVat->enc_value);       
+    }
+    
+    function test_creditOrderRows_handles_creditOrderRows_specified_using_incvat_and_vatpercent() {       
+        // needs either setRow(s)ToCredit or addCreditOrderRow(s)    
+        $creditOrder = WebPayAdmin::creditOrderRows( Svea\SveaConfig::getDefaultConfig() )
+            ->setInvoiceId("123456789")                
+            ->setInvoiceDistributionType(DistributionType::POST)
+            ->setCountryCode("SE")            
+            ->addCreditOrderRow( 
+                    WebPayItem::orderRow()
+                        ->setAmountIncVat(10.00)
+                        ->setVatPercent(25)
+                        ->setQuantity(1)
+            )
+        ;
+        $request = $creditOrder->creditInvoiceOrderRows()->prepareRequest();
+     
+        $this->assertEquals("10", $request->NewCreditInvoiceRows->enc_value[0]->enc_value->PricePerUnit->enc_value);
+        $this->assertEquals("25", $request->NewCreditInvoiceRows->enc_value[0]->enc_value->VatPercent->enc_value);
+        $this->assertEquals(true, $request->NewCreditInvoiceRows->enc_value[0]->enc_value->PriceIncludingVat->enc_value);    
+    }    
+    
+    function test_creditOrderRows_handles_creditOrderRows_specified_using_incvat_and_exvat() {       
+        // needs either setRow(s)ToCredit or addCreditOrderRow(s)    
+        $creditOrder = WebPayAdmin::creditOrderRows( Svea\SveaConfig::getDefaultConfig() )
+            ->setInvoiceId("123456789")                
+            ->setInvoiceDistributionType(DistributionType::POST)
+            ->setCountryCode("SE")            
+            ->addCreditOrderRow( 
+                    WebPayItem::orderRow()
+                        ->setAmountIncVat(12.50)
+                        ->setAmountExVat(10.00)
+                        ->setQuantity(1)
+            )
+        ;
+        $request = $creditOrder->creditInvoiceOrderRows()->prepareRequest();
+
+    $this->assertEquals("12.50", $request->NewCreditInvoiceRows->enc_value[0]->enc_value->PricePerUnit->enc_value);
+    $this->assertEquals("25", $request->NewCreditInvoiceRows->enc_value[0]->enc_value->VatPercent->enc_value);
+    $this->assertEquals("1", $request->NewCreditInvoiceRows->enc_value[0]->enc_value->PriceIncludingVat->enc_value);    
+    }
+    
+    
     
 }
