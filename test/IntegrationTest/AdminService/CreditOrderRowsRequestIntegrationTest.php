6efb77c52a971ef8c42ac8551b3b5c0af351339e
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2015-05-05 11:27:17 +0200

    CreditOrderRows handles new rows specified using exvat and incvat correctly, cleaned up tests

--- test/IntegrationTest/AdminService/CreditOrderRowsRequestIntegrationTest.php
+++ test/IntegrationTest/AdminService/CreditOrderRowsRequestIntegrationTest.php
@@ -163,10 +163,7 @@ class CreditOrderRowsRequestIntegrationTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals("24", $query->numberedOrderRows[0]->vatPercent);
     }
            
-    
-    
-   
-    /// characterizing tests for INTG-551
+    /// characterizing unit tests for INTG-551
     function test_creditOrderRows_handles_creditOrderRows_specified_using_exvat_and_vatpercent() {       
         // needs either setRow(s)ToCredit or addCreditOrderRow(s)    
         $creditOrder = WebPayAdmin::creditOrderRows( Svea\SveaConfig::getDefaultConfig() )
@@ -224,6 +221,115 @@ class CreditOrderRowsRequestIntegrationTest extends PHPUnit_Framework_TestCase {
 
     $this->assertEquals("12.50", $request->NewCreditInvoiceRows->enc_value[0]->enc_value->PricePerUnit->enc_value);
     $this->assertEquals("25", $request->NewCreditInvoiceRows->enc_value[0]->enc_value->VatPercent->enc_value);
-    $this->assertEquals("1", $request->NewCreditInvoiceRows->enc_value[0]->enc_value->PriceIncludingVat->enc_value);    
+    $this->assertEquals(true, $request->NewCreditInvoiceRows->enc_value[0]->enc_value->PriceIncludingVat->enc_value);    
     }  
+    
+    // INTG-551 integration tests
+    public function test_credit_row_sent_exandvat_credit_sent_incandex() {  // credit req. should be resent, see backoffice logs
+        $config = Svea\SveaConfig::getDefaultConfig();
+        
+        $orderInfo = $this->get_orderInfo_sent_ex_vat( 100.00, 25, 2 );
+
+        // query order and assert row totals
+        $query = WebPayAdmin::queryOrder($config)
+                ->setOrderId($orderInfo->orderId)
+                ->setCountryCode('SE')
+                ->queryInvoiceOrder()->doRequest();       
+        $this->assertEquals(1, $query->accepted);                
+        $this->assertEquals("100", $query->numberedOrderRows[0]->amountExVat);
+        $this->assertEquals(null, $query->numberedOrderRows[0]->amountIncVat);
+        $this->assertEquals("25", $query->numberedOrderRows[0]->vatPercent);            
+        $this->assertEquals("2", $query->numberedOrderRows[0]->quantity);            
+
+        $creditOrder = WebPayAdmin::creditOrderRows( $config )
+            ->setInvoiceId($orderInfo->invoiceId)                
+            ->setInvoiceDistributionType(DistributionType::POST)
+            ->setCountryCode("SE")            
+            ->addCreditOrderRow( 
+                    WebPayItem::orderRow()
+                        ->setAmountIncVat(20.00)
+                        ->setAmountExVat(16.00)
+                        ->setQuantity(1)
+            )
+        ;
+        $request = $creditOrder->creditInvoiceOrderRows()->prepareRequest();
+     
+        $this->assertEquals("20", $request->NewCreditInvoiceRows->enc_value[0]->enc_value->PricePerUnit->enc_value);
+        $this->assertEquals("25", $request->NewCreditInvoiceRows->enc_value[0]->enc_value->VatPercent->enc_value);
+        $this->assertEquals(true, $request->NewCreditInvoiceRows->enc_value[0]->enc_value->PriceIncludingVat->enc_value);           
+
+        $response = $creditOrder->creditInvoiceOrderRows()->doRequest();
+        $this->assertEquals(1, $response->accepted); 
+   
+        // query order and assert row totals
+        $query2 = WebPayAdmin::queryOrder($config)
+                ->setOrderId($orderInfo->orderId)
+                ->setCountryCode('SE')
+                ->queryInvoiceOrder()->doRequest();       
+        $this->assertEquals(1, $query2->accepted);   
+
+        // NOTE the order row status/amount does not reflect that the corresponding invoice row has been credited
+        // TODO implement queryInvoice and recurse invoices to get the current order row status
+        $this->assertEquals("100", $query2->numberedOrderRows[0]->amountExVat);
+        $this->assertEquals(null, $query2->numberedOrderRows[0]->amountIncVat);
+        $this->assertEquals("25", $query2->numberedOrderRows[0]->vatPercent);            
+        $this->assertEquals("2", $query2->numberedOrderRows[0]->quantity);           
+        // nope, can't be seen in the order, only in backoffice in delivered invoice as cumulative discount amount
+        // $this->assertEquals("-10.00", $query->numberedOrderRows[1]->amountExVat); 
+        // $this->assertEquals("25", $query->numberedOrderRows[1]->vatPercent);        
+    }    
+
+    public function test_credit_row_sent_inc_credit_sent_incandex() {  // credit req. should not be resent, see backoffice logs
+        $config = Svea\SveaConfig::getDefaultConfig();
+        
+        $orderInfo = $this->get_orderInfo_sent_inc_vat( 125.00, 25, 2 );
+
+        // query order and assert row totals
+        $query = WebPayAdmin::queryOrder($config)
+                ->setOrderId($orderInfo->orderId)
+                ->setCountryCode('SE')
+                ->queryInvoiceOrder()->doRequest();       
+        $this->assertEquals(1, $query->accepted);                
+        $this->assertEquals(null, $query->numberedOrderRows[0]->amountExVat);
+        $this->assertEquals("125", $query->numberedOrderRows[0]->amountIncVat);
+        $this->assertEquals("25", $query->numberedOrderRows[0]->vatPercent);            
+        $this->assertEquals("2", $query->numberedOrderRows[0]->quantity);            
+
+        $creditOrder = WebPayAdmin::creditOrderRows( $config )
+            ->setInvoiceId($orderInfo->invoiceId)                
+            ->setInvoiceDistributionType(DistributionType::POST)
+            ->setCountryCode("SE")            
+            ->addCreditOrderRow( 
+                    WebPayItem::orderRow()
+                        ->setAmountIncVat(20.00)
+                        ->setAmountExVat(16.00)
+                        ->setQuantity(1)
+            )
+        ;
+        $request = $creditOrder->creditInvoiceOrderRows()->prepareRequest();
+     
+        $this->assertEquals("20", $request->NewCreditInvoiceRows->enc_value[0]->enc_value->PricePerUnit->enc_value);
+        $this->assertEquals("25", $request->NewCreditInvoiceRows->enc_value[0]->enc_value->VatPercent->enc_value);
+        $this->assertEquals(true, $request->NewCreditInvoiceRows->enc_value[0]->enc_value->PriceIncludingVat->enc_value);           
+
+        $response = $creditOrder->creditInvoiceOrderRows()->doRequest();
+        $this->assertEquals(1, $response->accepted); 
+   
+        // query order and assert row totals
+        $query2 = WebPayAdmin::queryOrder($config)
+                ->setOrderId($orderInfo->orderId)
+                ->setCountryCode('SE')
+                ->queryInvoiceOrder()->doRequest();       
+        $this->assertEquals(1, $query2->accepted);   
+
+        // NOTE the order row status/amount does not reflect that the corresponding invoice row has been credited
+        // TODO implement queryInvoice and recurse invoices to get the current order row status
+        $this->assertEquals(null, $query2->numberedOrderRows[0]->amountExVat);
+        $this->assertEquals("125", $query2->numberedOrderRows[0]->amountIncVat);
+        $this->assertEquals("25", $query2->numberedOrderRows[0]->vatPercent);            
+        $this->assertEquals("2", $query2->numberedOrderRows[0]->quantity);           
+        // nope, can't be seen in the order, only in backoffice in delivered invoice as cumulative discount amount
+        // $this->assertEquals("-20.00", $query->numberedOrderRows[1]->amountExVat); 
+        // $this->assertEquals("25", $query->numberedOrderRows[1]->vatPercent);        
+    } 
 }
