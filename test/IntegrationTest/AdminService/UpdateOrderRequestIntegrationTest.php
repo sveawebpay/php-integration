f32d11509f51562915b32d781d684985be9cee4a
Anneli Halldn <anneli.halldn@sveaekonomi.se>
2015-08-27 16:03:12 +0200

    tests for updateOrder

---
+++ test/IntegrationTest/AdminService/UpdateOrderRequestIntegrationTest.php
@@ -0,0 +1,73 @@
+<?php
+
+$root = realpath(dirname(__FILE__));
+require_once $root . '/../../../src/Includes.php';
+require_once $root . '/../../TestUtil.php';
+
+/**
+ * @author Kristian Grossman-Madsen for Svea Webpay
+ */
+class UpdateOrderRequestIntegrationTest extends PHPUnit_Framework_TestCase {
+
+    public function test_add_new_clientordernumber_invoice() {
+        $config = Svea\SveaConfig::getDefaultConfig();
+        $orderResponse = WebPay::createOrder($config)
+                ->addOrderRow(
+                        WebPayItem::orderRow()
+                        ->setAmountIncVat(123.9876)
+                        ->setVatPercent(24)
+                        ->setQuantity(1)
+                )
+                ->addCustomerDetails(TestUtil::createIndividualCustomer("SE"))
+                ->setCountryCode("SE")
+                ->useInvoicePayment()->doRequest();
+        $this->assertEquals(1, $orderResponse->accepted);
+
+        $response = WebPayAdmin::updateOrder($config)
+                ->setCountryCode('SE')
+                ->setOrderId($orderResponse->sveaOrderId)
+                ->setClientOrderNumber('123')//string
+                ->updateInvoiceOrderRows()->doRequest();
+        $this->assertEquals(1, $response->accepted);
+
+        // query order and assert row totals
+        $query2 = WebPayAdmin::queryOrder($config)
+                ->setOrderId($orderResponse->sveaOrderId)
+                ->setCountryCode('SE')
+                ->queryInvoiceOrder()->doRequest();
+        $this->assertEquals(1, $query2->accepted);
+        $this->assertEquals('123', $query2->clientOrderNumber);
+        //print_r($orderResponse->sveaOrderId);
+    }
+    public function test_add_new_notes_invoice() {
+        $config = Svea\SveaConfig::getDefaultConfig();
+        $orderResponse = WebPay::createOrder($config)
+                ->addOrderRow(
+                        WebPayItem::orderRow()
+                        ->setAmountIncVat(123.9876)
+                        ->setVatPercent(24)
+                        ->setQuantity(1)
+                )
+                ->addCustomerDetails(TestUtil::createIndividualCustomer("SE"))
+                ->setCountryCode("SE")
+                ->useInvoicePayment()->doRequest();
+        $this->assertEquals(1, $orderResponse->accepted);
+
+        $response = WebPayAdmin::updateOrder($config)
+                ->setCountryCode('SE')
+                ->setOrderId($orderResponse->sveaOrderId)
+                ->setNotes('My notes 123')//string
+                ->updateInvoiceOrderRows()->doRequest();
+        $this->assertEquals(1, $response->accepted);
+
+        // query order and assert row totals
+        $query2 = WebPayAdmin::queryOrder($config)
+                ->setOrderId($orderResponse->sveaOrderId)
+                ->setCountryCode('SE')
+                ->queryInvoiceOrder()->doRequest();
+        $this->assertEquals(1, $query2->accepted);
+        $this->assertEquals('My notes 123', $query2->Notes);
+        //print_r($orderResponse->sveaOrderId);
+    }
+
+}
--- test/UnitTest/AdminService/UpdateOrderRequestTest.php
+++ test/UnitTest/AdminService/UpdateOrderRequestTest.php
@@ -15,24 +15,54 @@ class UpdateOrderRequestTest extends \PHPUnit_Framework_TestCase {
      * req: SveaOrderId, ClientId
      * update: Notes, ClientOrderNumber
      */
-    public function test_updateorder_clientnr() {
+    public function test_updateorder_clientnr_invoice() {
         $config = Svea\SveaConfig::getDefaultConfig();
         $request = WebPayAdmin::updateOrder($config)//only need clientid
-              ->setCountryCode('SE') //req for config
-              ->setOrderId('test')
-              ->updateInvoiceOrder()
-              ->prepareRequest();
+                ->setCountryCode('SE') //req for config
+                ->setOrderId('test')
+                ->setClientOrderNumber('123')//string
+                ->setNotes('My notes 123')//string
+                ->updateInvoiceOrder()
+                ->prepareRequest();
       print_r($request);
         $this->assertEquals(80, $request->UpdatedOrderRows->enc_value->enc_value[0]->enc_value->PricePerUnit->enc_value);
         $this->assertFalse($request->UpdatedOrderRows->enc_value->enc_value[0]->enc_value->PriceIncludingVat->enc_value);
     }
-    public function test_updateorder_addnotes() {
+    public function test_updateorder_addnotes_invoice() {
         $config = Svea\SveaConfig::getDefaultConfig();
         $request = WebPayAdmin::updateOrder($config)//only need clientid
-              ->setCountryCode('SE') //req for config
-              ->setOrderId('test')
-              ->updateInvoiceOrder()
-              ->prepareRequest();
+                ->setCountryCode('SE') //req for config
+                ->setOrderId('test')
+                ->setClientOrderNumber('123')//string
+                ->setNotes('My notes 123')//string
+                ->updateInvoiceOrder()
+                ->prepareRequest();
+      print_r($request);
+        $this->assertEquals(80, $request->UpdatedOrderRows->enc_value->enc_value[0]->enc_value->PricePerUnit->enc_value);
+        $this->assertFalse($request->UpdatedOrderRows->enc_value->enc_value[0]->enc_value->PriceIncludingVat->enc_value);
+    }
+    public function test_updateorder_clientnr_paymentplan() {
+        $config = Svea\SveaConfig::getDefaultConfig();
+        $request = WebPayAdmin::updateOrder($config)//only need clientid
+                ->setCountryCode('SE') //req for config
+                ->setOrderId('test')
+                ->setClientOrderNumber('123')//string
+                ->setNotes('My notes 123')//string
+                ->updateInvoiceOrder()
+                ->prepareRequest();
+      print_r($request);
+        $this->assertEquals(80, $request->UpdatedOrderRows->enc_value->enc_value[0]->enc_value->PricePerUnit->enc_value);
+        $this->assertFalse($request->UpdatedOrderRows->enc_value->enc_value[0]->enc_value->PriceIncludingVat->enc_value);
+    }
+    public function test_updateorder_addnotes_invoice_paymentplan() {
+        $config = Svea\SveaConfig::getDefaultConfig();
+        $request = WebPayAdmin::updateOrder($config)//only need clientid
+                ->setCountryCode('SE') //req for config
+                ->setOrderId('test')
+                ->setClientOrderNumber('123')//string
+                ->setNotes('My notes 123')//string
+                ->updateInvoiceOrder()
+                ->prepareRequest();
       print_r($request);
         $this->assertEquals(80, $request->UpdatedOrderRows->enc_value->enc_value[0]->enc_value->PricePerUnit->enc_value);
         $this->assertFalse($request->UpdatedOrderRows->enc_value->enc_value[0]->enc_value->PriceIncludingVat->enc_value);
