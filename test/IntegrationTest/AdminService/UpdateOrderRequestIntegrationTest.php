74b7ebe7ee028af2c7dc32d8803d44e317bfb1cd
Anneli Halldn <anneli.halldn@sveaekonomi.se>
2015-09-01 16:41:25 +0200

    fix broken tests

--- test/IntegrationTest/AdminService/UpdateOrderRequestIntegrationTest.php
+++ test/IntegrationTest/AdminService/UpdateOrderRequestIntegrationTest.php
@@ -9,6 +9,10 @@ require_once $root . '/../../TestUtil.php';
  */
 class UpdateOrderRequestIntegrationTest extends PHPUnit_Framework_TestCase {
 
+    public $notes = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                    Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+                    penatibus et magnis';
+
     public function test_add_new_clientordernumber_invoice() {
         $config = Svea\SveaConfig::getDefaultConfig();
                $orderResponse = WebPay::createOrder($config)
@@ -61,10 +65,7 @@ class UpdateOrderRequestIntegrationTest extends PHPUnit_Framework_TestCase {
         $response = WebPayAdmin::updateOrder($config)
                 ->setCountryCode('SE')
                 ->setOrderId($orderResponse->sveaOrderId)
-                ->setNotes('Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-                    penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                    Donec qu')//string 200 chars
+                ->setNotes($this->notes)//string 200 chars
                 ->updateInvoiceOrder()->doRequest();
         $this->assertEquals(1, $response->accepted);
 
@@ -74,48 +75,8 @@ class UpdateOrderRequestIntegrationTest extends PHPUnit_Framework_TestCase {
                 ->setCountryCode('SE')
                 ->queryInvoiceOrder()->doRequest();
         $this->assertEquals(1, $query2->accepted);
-        $this->assertEquals('Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-                    penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                    Donec qu', $query2->notes);
+        $this->assertEquals($this->notes, $query2->notes);
         //print_r($orderResponse->sveaOrderId);
-    }
-
-    //TODO: Where to put this test
-
-    public function test_add_new_notes_too_long_invoice() {
-        $config = Svea\SveaConfig::getDefaultConfig();
-        $orderResponse = WebPay::createOrder($config)
-                ->addOrderRow(
-                        WebPayItem::orderRow()
-                        ->setAmountExVat(145.00)
-                        ->setVatPercent(24)
-                        ->setQuantity(1)
-                )
-                ->addCustomerDetails(TestUtil::createIndividualCustomer("SE"))
-                ->setCountryCode("SE")
-                ->setOrderDate("2012-12-12")
-                ->useInvoicePayment()
-                ->doRequest();
-        $this->assertEquals(1, $orderResponse->accepted);
-
-        try {
-                    $response = WebPayAdmin::updateOrder($config)
-                ->setCountryCode('SE')
-                ->setOrderId($orderResponse->sveaOrderId)
-                ->setNotes('Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-                    penatibus et magnis dis parturient montes, nascetur ridiculus mus. D
-                    onec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
-                    Nulla consequat massa quis enim. Donec.')//string 300 chars
-                ->updateInvoiceOrder()->doRequest();
-        } catch (Exception $exc) {
-            print_r($exc->getMessage());
-        }
-
-        $this->assertEquals(1, $response->accepted);
-
-
     }
 
 }
