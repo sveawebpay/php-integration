15c5e48f2ee2c33fa9ab1a60660b5142f2b84045
Fredrik Sundell <fredrik.sundell@svea.com>
2019-09-11 13:56:36 +0200

    Avoid calling calculateVatPercentFromVatAndAmount statically in unit tests

--- test/UnitTest/HostedService/HandleOrder/QueryTransactionResponseTest.php
+++ test/UnitTest/HostedService/HandleOrder/QueryTransactionResponseTest.php
@@ -10,21 +10,27 @@ class QueryTransactionResponseTest extends \PHPUnit\Framework\TestCase
 {
     function test_calculateVatPercentFromVatAndAmount_returns_correct_vatpercent_if_transactions_contains_negative_row()
     {
-        $test = QueryTransactionResponse::calculateVatPercentFromVatAndAmount("-1252", "-6262");
+        $queryTransactionResponse = new QueryTransactionResponse("", "", "");
+
+        $test = $queryTransactionResponse->calculateVatPercentFromVatAndAmount("-1252", "-6262");
 
         $this->assertEquals("25.00", $test);
     }
 
     function test_calculateVatPercentFromVatAndAmount_returns_correct_vatpercent_if_transactions_contains_positive_row()
     {
-        $test = QueryTransactionResponse::calculateVatPercentFromVatAndAmount("1252", "6262");
+        $queryTransactionResponse = new QueryTransactionResponse("", "", "");
+
+        $test = $queryTransactionResponse->calculateVatPercentFromVatAndAmount("1252", "6262");
 
         $this->assertEquals("25.00", $test);
     }
 
     function test_calculateVatPercentFromVatAndAmount_returns_correct_vatpercent_if_transactions_contains_zero_amount_row()
     {
-        $test = QueryTransactionResponse::calculateVatPercentFromVatAndAmount("0", "0");
+        $queryTransactionResponse = new QueryTransactionResponse("", "", "");
+
+        $test = $queryTransactionResponse->calculateVatPercentFromVatAndAmount("0", "0");
 
         $this->assertEquals("0.00", $test);
     }
